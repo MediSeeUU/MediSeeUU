4ea92cb2275048ad6fa532bd1b62d458bb3e7682
Rutger <rutgerdj2@gmail.com>
2022-03-21 13:16:26 +0100

    Move files into folder with no `-` in name

--- medctrl-backend/API/API_settings/urls.py
+++ medctrl-backend/API/API_settings/urls.py
@@ -1,4 +1,4 @@
-"""API-settings URL Configuration
+"""API_settings URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
