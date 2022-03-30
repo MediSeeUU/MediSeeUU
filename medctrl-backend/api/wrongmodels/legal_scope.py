ab6c7fa810ba1a79c637808a5059924c72315ef9
Asehujiko <Asehujiko_@hotmail.com>
2022-03-30 13:54:27 +0200

    added vs to gitignore, cleaned up wrongmodels

--- medctrl-backend/api/wrongmodels/legal_scope.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class LegalScope(models.Model):
-    legal_scope_id = models.IntegerField(primary_key=True)
-    description = models.CharField(max_length=45)
-
-    class Meta:
-        managed = False
-        db_table = 'legal_scope'
