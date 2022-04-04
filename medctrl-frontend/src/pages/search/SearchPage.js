5bad077873d3734df21ef5a42c469f83e62646d3
ManoukSirag <Siragmanouk@gmail.com>
2022-04-04 16:54:45 +0200

    initial searchpage stuff
    includes the table, a search bar and some improvsed button that needs to be changed later

--- medctrl-frontend/src/pages/search/SearchPage.js
+++ medctrl-frontend/src/pages/search/SearchPage.js
@@ -1,9 +1,44 @@
+import './SearchPage.css'
+import Table from '../../shared/table/table'
+import DummyData from '../../json/data.json'
+
 function SearchPage() {
+
+  const allData = DummyData
+
   return (
     <div>
-      <h1>Search!</h1>
+
+      <div className="content__container__top">
+        {/* <label>Active table settings</label> */}
+        <button className="tableButtons">
+        <i className="bx bx-cog filter-Icon"></i>Filter & Sort
+        </button>
+        <hr></hr>
+        
+        <input type="text" placeholder="Search" className='content__container__textinput' onChange={test()} />
+
+
+      </div>
+
+      <div className="content__container">
+        <h1>Results</h1>
+
+        <Table
+          data={allData}
+          currentPage={1}
+          amountPerPage={3}
+          selectTable={false}
+          />
+        
+
+      </div>
     </div>
   )
+}
+
+function test() {
+  console.log("test");
 }
 
 export default SearchPage
