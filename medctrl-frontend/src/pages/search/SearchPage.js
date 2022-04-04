5770b9b4be54f708ba472da4b38a40132a65e203
ManoukSirag <Siragmanouk@gmail.com>
2022-04-04 18:12:05 +0200

    Searchpage layout finished
    updated the search page with new layout designs

--- medctrl-frontend/src/pages/search/SearchPage.js
+++ medctrl-frontend/src/pages/search/SearchPage.js
@@ -3,42 +3,38 @@ import Table from '../../shared/table/table'
 import DummyData from '../../json/data.json'
 
 function SearchPage() {
-
   const allData = DummyData
 
   return (
     <div>
-
-      <div className="content__container__top">
-        {/* <label>Active table settings</label> */}
-        <button className="tableButtons">
-        <i className="bx bx-cog filter-Icon"></i>Filter & Sort
+      <div className="TopTableHolder">
+        <button className="searchbox__button">
+          <i className="bx bx-cog filter-Icon"></i>Filter & Sort
         </button>
-        <hr></hr>
-        
-        <input type="text" placeholder="Search" className='content__container__textinput' onChange={test()} />
-
-
+        <input
+          type="text"
+          placeholder="Search"
+          className="content__container__textinput"
+          onChange={(e) => test(e.target.value)}
+        />
       </div>
 
-      <div className="content__container">
+      <div className="TopTableHolder">
         <h1>Results</h1>
 
         <Table
           data={allData}
           currentPage={1}
-          amountPerPage={3}
+          amountPerPage={50}
           selectTable={false}
-          />
-        
-
+        />
       </div>
     </div>
   )
 }
 
-function test() {
-  console.log("test");
+function test(ding) {
+  console.log(ding)
 }
 
 export default SearchPage
