830385f030d2f39d29c83ec38c27e23720fc3efa
Siebe Vuijst <s.vuijst@students.uu.nl>
2022-04-04 19:34:56 +0200

    Fixed linting errors in unit tests

--- medctrl-frontend/src/shared/menu/__test__/Menu.test.js
+++ medctrl-frontend/src/shared/menu/__test__/Menu.test.js
@@ -17,60 +17,50 @@ test('renders without crashing', () => {
 })
 
 test('opens menu after clicking button', () => {
-  const { getByText, getByLabelText, queryByLabelText } = render(
-    <Menu cachedData={DummyData} />
-  )
-  expect(queryByLabelText(/Menu/i)).not.toBeInTheDocument()
-  fireEvent.click(getByText(/Filter & Sort/i))
-  expect(getByLabelText(/Menu/i)).toBeInTheDocument()
+  render(<Menu cachedData={DummyData} />)
+  expect(screen.queryByLabelText(/Menu/i)).not.toBeInTheDocument()
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  expect(screen.getByLabelText(/Menu/i)).toBeInTheDocument()
 })
 
 test('apply button calls update function', () => {
   const update = jest.fn()
-  const { getByText } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
   expect(update).not.toHaveBeenCalled()
-  fireEvent.click(getByText(/Apply/i))
+  fireEvent.click(screen.getByText(/Apply/i))
   expect(update).toHaveBeenCalled()
 })
 
 test('clear button calls update function', () => {
   const update = jest.fn()
-  const { getByText } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
   expect(update).not.toHaveBeenCalled()
-  fireEvent.click(getByText(/Clear/i))
+  fireEvent.click(screen.getByText(/Clear/i))
   expect(update).toHaveBeenCalled()
 })
 
 test('clear button resets data', () => {
   const update = (data) => expect(data).toBe(DummyData)
-  const { getByText } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
-  fireEvent.click(getByText(/Clear/i))
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  fireEvent.click(screen.getByText(/Clear/i))
 })
 
 test('close button closes menu', () => {
-  const { getByText, queryByLabelText } = render(
-    <Menu cachedData={DummyData} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
-  fireEvent.click(getByText(/Close/i))
-  expect(queryByLabelText(/Menu/i)).not.toBeInTheDocument()
+  render(<Menu cachedData={DummyData} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  fireEvent.click(screen.getByText(/Close/i))
+  expect(screen.queryByLabelText(/Menu/i)).not.toBeInTheDocument()
 })
 
 test('add filter adds filter item', () => {
-  const { getByText, queryAllByRole } = render(<Menu cachedData={DummyData} />)
-  fireEvent.click(getByText(/Filter & Sort/i))
-  expect(queryAllByRole('combobox')).toHaveLength(1)
-  fireEvent.click(getByText(/Add Filter/i))
-  expect(queryAllByRole('combobox')).toHaveLength(2)
+  render(<Menu cachedData={DummyData} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  expect(screen.queryAllByRole('combobox')).toHaveLength(1)
+  fireEvent.click(screen.getByText(/Add Filter/i))
+  expect(screen.queryAllByRole('combobox')).toHaveLength(2)
 })
 
 test('single filter applied correctly', () => {
@@ -79,16 +69,14 @@ test('single filter applied correctly', () => {
       expect(element.ApplicationNo.toString()).toContain('8')
     })
   }
-  const { getByText, getByRole } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
-  const select = getByRole('combobox')
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const select = screen.getByRole('combobox')
   fireEvent.change(select, { target: { value: 'ApplicationNo' } })
-  const textBox = getByRole('textbox')
+  const textBox = screen.getByRole('textbox')
   fireEvent.change(textBox, { target: { value: '8' } })
   fireEvent.focusOut(textBox)
-  fireEvent.click(getByText(/Apply/i))
+  fireEvent.click(screen.getByText(/Apply/i))
 })
 
 test('two filters applied correctly', () => {
@@ -98,22 +86,20 @@ test('two filters applied correctly', () => {
       expect(element.DecisionYear.toString()).toContain('2001')
     })
   }
-  const { getByText, getByRole, getAllByRole } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
-  const firstSelect = getByRole('combobox')
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const firstSelect = screen.getByRole('combobox')
   fireEvent.change(firstSelect, { target: { value: 'ApplicationNo' } })
-  const firstText = getByRole('textbox')
+  const firstText = screen.getByRole('textbox')
   fireEvent.change(firstText, { target: { value: '7' } })
   fireEvent.focusOut(firstText)
-  fireEvent.click(getByText(/Add Filter/i))
-  const secondSelect = getAllByRole('combobox')[1]
+  fireEvent.click(screen.getByText(/Add Filter/i))
+  const secondSelect = screen.getAllByRole('combobox')[1]
   fireEvent.change(secondSelect, { target: { value: 'DecisionYear' } })
-  const secondText = getAllByRole('textbox')[1]
+  const secondText = screen.getAllByRole('textbox')[1]
   fireEvent.change(secondText, { target: { value: '2001' } })
   fireEvent.focusOut(secondText)
-  fireEvent.click(getByText(/Apply/i))
+  fireEvent.click(screen.getByText(/Apply/i))
 })
 
 test('multiple values in filter applied correctly', () => {
@@ -122,31 +108,29 @@ test('multiple values in filter applied correctly', () => {
       expect(element.DecisionYear.toString()).toMatch(/(1997|2001)/i)
     })
   }
-  const { getByText, getByRole, getAllByRole } = render(
-    <Menu cachedData={DummyData} updateTable={update} />
-  )
-  fireEvent.click(getByText(/Filter & Sort/i))
-  const firstSelect = getByRole('combobox')
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const firstSelect = screen.getByRole('combobox')
   fireEvent.change(firstSelect, { target: { value: 'DecisionYear' } })
-  const firstText = getByRole('textbox')
+  const firstText = screen.getByRole('textbox')
   fireEvent.change(firstText, { target: { value: '1997' } })
   fireEvent.focusOut(firstText)
-  fireEvent.click(getByText('+ Add'))
-  const secondText = getAllByRole('textbox')[1]
+  fireEvent.click(screen.getByText('+ Add'))
+  const secondText = screen.getAllByRole('textbox')[1]
   fireEvent.change(secondText, { target: { value: '2001' } })
   fireEvent.focusOut(secondText)
-  fireEvent.click(getByText(/Apply/i))
+  fireEvent.click(screen.getByText(/Apply/i))
 })
 
 test('saved filters in state', () => {
-  const { getByText, getByRole } = render(<Menu cachedData={DummyData} />)
-  fireEvent.click(getByText(/Filter & Sort/i))
-  const select = getByRole('combobox')
+  render(<Menu cachedData={DummyData} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const select = screen.getByRole('combobox')
   fireEvent.change(select, { target: { value: 'ApplicationNo' } })
-  const text = getByRole('textbox')
+  const text = screen.getByRole('textbox')
   fireEvent.change(text, { target: { value: '10' } })
-  fireEvent.click(getByText(/Close/i))
-  fireEvent.click(getByText(/Filter & Sort/i))
+  fireEvent.click(screen.getByText(/Close/i))
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
   expect(select.value).toBe('ApplicationNo')
   expect(text.value).toBe('10')
 })
