802ac57453e637867acae364cd0d544e4f122a03
Siebe Vuijst <s.vuijst@students.uu.nl>
2022-04-06 13:32:02 +0200

    Fixed linting errors in unit tests

--- medctrl-frontend/src/shared/table/__test__/table.test.js
+++ medctrl-frontend/src/shared/table/__test__/table.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { render, fireEvent, within } from '@testing-library/react'
+import { render, screen, fireEvent, within } from '@testing-library/react'
 import Table from '../table'
 import DummyData from '../../../json/data.json'
 
@@ -13,58 +13,54 @@ test('renders without crashing', () => {
 })
 
 test('expected amount of rows in the table', () => {
-  const view = render(
+  render(
     <Table
       data={DummyData}
       currentPage={1}
       amountPerPage={DummyData.length + 10}
     />
   )
-  const table = view.getByRole('table')
+  const table = screen.getByRole('table')
   const rows = within(table).getAllByRole('row')
   expect(rows).toHaveLength(DummyData.length + 1)
 })
 
 test('expected amount of headers in the table', () => {
-  const view = render(
-    <Table data={DummyData} currentPage={1} amountPerPage={10} />
-  )
-  const table = view.getByRole('table')
+  render(<Table data={DummyData} currentPage={1} amountPerPage={10} />)
+  const table = screen.getByRole('table')
   const rows = within(table).getAllByRole('row')
   const headers = within(rows[0]).queryAllByRole('columnheader')
   expect(headers).toHaveLength(Object.keys(DummyData[0]).length)
 })
 
 test('checkboxes displayed', () => {
-  const data = DummyData
-  const view = render(
+  render(
     <Table
-      data={data}
+      data={DummyData}
       selectTable={true}
       setCheckedState={() => {}}
-      checkedState={Array(data.length).fill(false)}
+      checkedState={Array(DummyData.length).fill(false)}
       currentPage={1}
       amountPerPage={10}
     />
   )
-  const table = view.getByRole('table')
-  const checkboxes = table.getElementsByClassName('tableCheckboxColumn')
+  const table = screen.getByRole('table')
+  const checkboxes = within(table).getAllByRole('checkbox')
   expect(checkboxes).toHaveLength(11)
 })
 
 test('row selected, when checkbox clicked', () => {
-  const data = DummyData
   let checkedState = Object.assign(
     {},
-    ...data.map((entry) => ({ [entry.EUNumber]: false }))
+    ...DummyData.map((entry) => ({ [entry.EUNumber]: false }))
   )
   const setCheckedState = (newState) => {
     checkedState = newState
   }
 
-  const view = render(
+  render(
     <Table
-      data={data}
+      data={DummyData}
       selectTable={true}
       setCheckedState={setCheckedState}
       checkedState={checkedState}
@@ -72,10 +68,10 @@ test('row selected, when checkbox clicked', () => {
       amountPerPage={10}
     />
   )
-  const table = view.getByRole('table')
-  const input = table.getElementsByClassName('tableCheckboxColumn')[1]
+  const table = screen.getByRole('table')
+  const input = within(table).getAllByRole('checkbox')[1]
   fireEvent.click(input)
-  expect(checkedState[data[10].EUNumber]).toBe(true)
+  expect(checkedState[DummyData[10].EUNumber]).toBe(true)
 })
 
 test('all rows selected when select all pressed', () => {
@@ -88,7 +84,7 @@ test('all rows selected when select all pressed', () => {
     checkedState = newState
   }
 
-  const view = render(
+  render(
     <Table
       data={data}
       selectTable={true}
@@ -98,8 +94,8 @@ test('all rows selected when select all pressed', () => {
       amountPerPage={10}
     />
   )
-  const table = view.getByRole('table')
-  const input = table.getElementsByClassName('tableCheckboxColumn')[0]
+  const table = screen.getByRole('table')
+  const input = within(table).getAllByRole('checkbox')[0]
   fireEvent.click(input)
   let checkedCount = () => {
     let count = 0
@@ -198,7 +194,7 @@ test('throw error when current page does not exist', () => {
 })
 
 test('data put correctly into table', () => {
-  const view = render(
+  render(
     <Table
       data={DummyData}
       selectTable={true}
@@ -208,8 +204,8 @@ test('data put correctly into table', () => {
       amountPerPage={10}
     />
   )
-  const headers = view.getAllByRole('columnheader')
-  const rowgroup = view.getAllByRole('rowgroup')[1]
+  const headers = screen.getAllByRole('columnheader')
+  const rowgroup = screen.getAllByRole('rowgroup')[1]
   const rows = within(rowgroup).getAllByRole('row')
   headers.forEach((header, index) => {
     const select = within(header).getByRole('combobox')
@@ -224,7 +220,7 @@ test('data put correctly into table', () => {
 })
 
 test('column change changes data in row', () => {
-  const view = render(
+  render(
     <Table
       data={DummyData}
       selectTable={true}
@@ -234,14 +230,14 @@ test('column change changes data in row', () => {
       amountPerPage={10}
     />
   )
-  const headers = view.queryAllByRole('columnheader')
+  const headers = screen.queryAllByRole('columnheader')
   const firstSelect = within(headers[0]).getByRole('combobox')
   const startValue = firstSelect.value
   const options = within(firstSelect).getAllByRole('option')
   const newValue = options[1].value
   expect(startValue).not.toBe(newValue)
   fireEvent.change(firstSelect, { target: { value: newValue } })
-  const rowgroup = view.getAllByRole('rowgroup')[1]
+  const rowgroup = screen.getAllByRole('rowgroup')[1]
   const rows = within(rowgroup).getAllByRole('row')
   rows.forEach((row, index) => {
     const cells = within(row).getAllByRole('cell')
