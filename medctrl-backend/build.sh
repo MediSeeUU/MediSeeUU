f8d38c793d10da5e5f961a163dbd9001caa6e9cd
Rutger <rutgerdj2@gmail.com>
2022-04-06 13:04:07 +0200

    Ensure python3.10 is used when buliding

--- medctrl-backend/build.sh
+++ medctrl-backend/build.sh
@@ -28,9 +28,9 @@ function print_highlight {
 # Setup a python environment
 function setup_python_env {
     print_highlight "Setting up python environment"
-    python3 -m pip install --upgrade pip
-    python3 -m pip install virtualenv
-    python3 -m virtualenv virtualenv
+    python3.10 -m pip install --upgrade pip
+    python3.10 -m pip install virtualenv
+    python3.10 -m virtualenv virtualenv
 
     source virtualenv/bin/activate
 
