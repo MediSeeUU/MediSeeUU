91e48e54da0801d6b11f799a7f13f243273a0097
A. Hartog <kiwisco@hotmail.com>
2022-04-08 17:12:02 +0200

    Basic sorting tests (to be extended)

--- medctrl-frontend/src/shared/menu/__test__/Menu.test.js
+++ medctrl-frontend/src/shared/menu/__test__/Menu.test.js
@@ -10,6 +10,7 @@ import {
 } from '@testing-library/react'
 import Menu from '../menu'
 import DummyData from '../../../json/data.json'
+import { convertSortingAttributeNameToComparisonFunction } from '../sorting'
 
 test('renders without crashing', () => {
   const root = document.createElement('div')
@@ -63,6 +64,9 @@ test('add filter adds filter item', () => {
   expect(screen.queryAllByTestId('filter-select')).toHaveLength(2)
 })
 
+
+
+
 test('single filter applied correctly', () => {
   const update = (data) => {
     data.forEach((element) => {
@@ -78,6 +82,178 @@ test('single filter applied correctly', () => {
   fireEvent.focusOut(textBox)
   fireEvent.click(screen.getByText(/Apply/i))
 })
+// test for checking if clicking the "Add sorting option" label adds an sorting-item (A)
+test('add sorting option adds sorting item', () => {
+  render(<Menu cachedData={DummyData} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  expect(screen.queryAllByTestId('sort-select-attr')).toHaveLength(1)
+  fireEvent.click(screen.getByText(/Add Sorting option +/i))
+  expect(screen.queryAllByTestId('sort-select-attr')).toHaveLength(2)
+})
+//test to check if single parameter sorting ascending on ApplicationNo (number) works
+test('single ascending sorter on ApplicationNo applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = convertSortingAttributeNameToComparisonFunction("ApplicationNo")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      var comparisonvalue = compareFunc(data[i], data[i+1])
+      expect(comparisonvalue).toBeLessThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "ApplicationNo" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "asc" } })
+
+  // temp neccessarry to change filter state, otherwise no sorting is applied
+  const select = screen.queryByTestId('filter-select')
+  fireEvent.change(select, { target: { value: 'CMA' } })
+  const textBox = screen.getByRole('textbox')
+  fireEvent.change(textBox, { target: { value: 'no' } })
+  fireEvent.focusOut(textBox)
+  //
+
+  fireEvent.click(screen.getByText(/Apply/i))
+})
+
+test('single descending sorter on ApplicationNo applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = convertSortingAttributeNameToComparisonFunction("ApplicationNo")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      var comparisonvalue = compareFunc(data[i], data[i+1])
+      expect(comparisonvalue).toBeGreaterThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "ApplicationNo" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "desc" } })
+
+  // temp neccessarry to change filter state, otherwise no sorting is applied
+  const select = screen.queryByTestId('filter-select')
+  fireEvent.change(select, { target: { value: 'CMA' } })
+  const textBox = screen.getByRole('textbox')
+  fireEvent.change(textBox, { target: { value: 'no' } })
+  fireEvent.focusOut(textBox)
+  //
+
+  fireEvent.click(screen.getByText(/Apply/i))
+})
+
+test('single ascending sorter on DecisionDate applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = convertSortingAttributeNameToComparisonFunction("DecisionDate")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      var comparisonvalue = compareFunc(data[i], data[i+1])
+      expect(comparisonvalue).toBeLessThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "DecisionDate" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "asc" } })
+
+  // temp neccessarry to change filter state, otherwise no sorting is applied
+  const select = screen.queryByTestId('filter-select')
+  fireEvent.change(select, { target: { value: 'CMA' } })
+  const textBox = screen.getByRole('textbox')
+  fireEvent.change(textBox, { target: { value: 'no' } })
+  fireEvent.focusOut(textBox)
+  //
+
+  fireEvent.click(screen.getByText(/Apply/i))
+})
+/* //test to check if sorting ascending on DecisionDate (special sorting case) works
+test('single sorter on DecisionDate applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = getSortingFunctionFromAttributeName("DecisionDate")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      expect(compareFunc(data[i]["DecisionDate"], data[i+1]["DecisionDate"])).toBeLessThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "DecisionDate" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "asc" } })
+  fireEvent.click(screen.getByText(/Apply/i))
+})
+
+//test to check if sorting descending on MAH (special sorting case) works
+test('single sorter on MAH applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = getSortingFunctionFromAttributeName("MAH")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      expect(compareFunc(data[i]["MAH"], data[i+1]["MAH"])).toBeMoreThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "MAH" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "desc" } })
+  fireEvent.click(screen.getByText(/Apply/i))
+})
+
+//test to check if sorting descending on ActiveSubstance (special sorting case) works
+test('single sorter on ActiveSubstance applied correctly', () => {
+  const update = (data) => {
+    var compareFunc = getSortingFunctionFromAttributeName("ActiveSubstance")
+    for(var i=0; i<Object.keys(data).length-1; i++)
+    {
+      expect(compareFunc(data[i]["ActiveSubstance"], data[i+1]["ActiveSubstance"])).toBeMoreThanOrEqual(0)
+    } 
+  }
+  render(<Menu cachedData={DummyData} updateTable={update} />)
+  fireEvent.click(screen.getByText(/Filter & Sort/i))
+  const selectedAttribute = screen.queryByTestId('sort-select-attr')
+  fireEvent.change(selectedAttribute, { target: { value: "ActiveSubstance" } })
+  const selectedOrder = screen.getByTestId('sort-select-order')
+  fireEvent.change(selectedOrder, { target: { value: "desc" } })
+  fireEvent.click(screen.getByText(/Apply/i))
+}) */
+// data-testid="sort-select-order" data-testid="sort-select-attr"
+/* // replace by individual attribute sorting tests=> unit tests
+// make sorting checkkk data-testid="sort-select-order" data-testid="sort-select-attr"
+test('single sorter applied correctly', () => {
+
+  function geupdateDataCheck(data, attribuut, sortingorder) {
+    var sortFuncForAttribute = getSortingFunctionFromAttributeName(attribuut)
+
+    for(var i=0; i< data.Length-1; i++ )
+    {
+      expect(sortFuncForAttribute(DummyData[i], DummyData[i+1])).toBelessThanOrEqual(0)
+    }
+  }
+  var sortingorders = ["asc", "desc"]
+  var allattributes = Object.keys(DummyData[0])
+  allattributes.forEach((attribuut) => {
+
+    sortingorders.forEach((sortingorder) =>{
+      
+      render(<Menu cachedData={DummyData} updateTable={geupdateDataCheck(this.props.data, attribuut, sortingorder)} />)
+      fireEvent.click(screen.getByText(/Filter & Sort/i))
+      const selectedAttribute = screen.queryByTestId('sort-select-attr')
+      fireEvent.change(selectedAttribute, { target: { value: attribuut } })
+      const selectedOrder = screen.getByTestId('sort-select-order')
+      fireEvent.change(selectedOrder, { target: { value: sortingorder } })
+      fireEvent.click(screen.getByText(/Apply/i))
+    })
+  })
+})
+// */
 
 test('two filters applied correctly', () => {
   const update = (data) => {
--- medctrl-frontend/src/shared/menu/sorting.js
+++ medctrl-frontend/src/shared/menu/sorting.js
@@ -6,6 +6,16 @@ import voca from 'voca'
 //one sorting filter object consists of 2 properties, { selected, order}. With selected being a string containing the name of the attribute to sort on.
 // order contains a string of either "asc" or "desc", for ascending or descending order
 export function sortData(data, sortingparameters) {
+  
+  for(var sortparIndex=0; sortparIndex<sortingparameters.length; sortparIndex++)
+  {
+    if(sortingparameters[sortparIndex].selected === '')
+    {sortingparameters.splice(sortparIndex,1)}
+  }
+  if(sortingparameters.length === 0 )
+  {return data}
+
+
   let initialComparisonFunction =
     convertSortingAttributeNameToComparisonFunction(
       sortingparameters[0].selected
@@ -31,21 +41,43 @@ export function sortData(data, sortingparameters) {
 }
 
 function createComparisonFunction(attr) {
-  return function compareAlphabetical(object1, object2) {
+  console.log(attr)
+  if(attr === undefined || attr === ''){
+      return function baseComparison(jsonObject1, jsonObject2){
+          jsonObject1[0].toString().localeCompare(jsonObject2[0].toString())
+      }
+    }
+  else{
+    return function alphanumericalcomparison(jsonobject1, jsonobject2){
+      console.log(attr + "____"+ jsonobject1[attr] + "____"+ jsonobject1.toString()) // logt getallen id voor ApplicationNo
+      return convertStringToAlphaNumerical(jsonobject1[attr]).localeCompare(
+        convertStringToAlphaNumerical(jsonobject2[attr]))
+    }
+  }
+  //return convertStringToAlphaNumerical(object1[attr]).localeCompare(
+  //  convertStringToAlphaNumerical(object2[attr])
+  //)
+
+  /* return function compareAlphabetical(object1, object2) {
       if(attr === undefined || attr === ''){
         return function baseComparison(jsonObject1, jsonObject2){
             jsonObject1[0].toString().localeCompare(jsonObject2[0].toString())
         }
       }
-    if (typeof object1[attr] == 'number') {
+    if (typeof object1[attr] === "number") {
       return object1[attr] - object2[attr]
     }
     return convertStringToAlphaNumerical(object1[attr]).localeCompare(
       convertStringToAlphaNumerical(object2[attr])
     )
-  }
+  } */
 }
 function convertStringToAlphaNumerical(word) {
+  console.log(word)
+  if (typeof word === 'Number')
+  {word = Number.toString(word)
+   console.log("!!!!!!"+ word)
+  }
   var lowercaselatinizedWord = voca.latinise(word.toString().toLowerCase())
   var AlphaNumericOnlyLatinizedWord = lowercaselatinizedWord.replace(
     /[^a-zA-Z0-9]+/g,
@@ -53,7 +85,7 @@ function convertStringToAlphaNumerical(word) {
   )
   return AlphaNumericOnlyLatinizedWord
 }
-
+//-----------------------------------------------------------------------------------------------------------------------
 //function takes one attributeName as a string, and returns the corresponding comparison function for json objects.
 //use this returned comparison function as an argument to .sort function of an array containing jsonObjects.
 export function convertSortingAttributeNameToComparisonFunction(
@@ -131,7 +163,7 @@ export function convertSortingAttributeNameToComparisonFunction(
   return sortingFunctionToUse
 }
 
-
+//----------------------------------------------------------------------------------------------------------------------------------
 //Function takes the name of an attribute as a string, returns the appropriate sorting function
 //use this function to sort an array containing only values of the provided AttributeName type.
 //use like: arrayName.sort(comparisonFunctionAsReturnedByThisFunction)
@@ -207,7 +239,21 @@ export function getSortingFunctionFromAttributeName(
 //function takes an attribute name and returns the correpsonding comparison function,
 //base clause for when no other match is found in getSortingFunctionFromAttributeName(attributeName)
 function createComparisonFunction2(attr) {
-return function compareAlphabetical(value1, value2) {
+  
+    if(attr === undefined || attr === ''){
+        return function baseComparison(value1, value2){
+            return value1.toString().localeCompare(value2.toString())
+        }
+    }
+    if (typeof value1 == 'number') {
+    return function basenumbercompare(value1, value2){ return (value1 - value2)}
+    }
+    return function alphanumericalcompare (value1, value2) {
+      return convertStringToAlphaNumerical(value1).localeCompare(convertStringToAlphaNumerical(value2))
+    }
+    
+  
+/* return function compareAlphabeticalA(value1, value2) {
     if(attr === undefined || attr === ''){
         return function baseComparison(value1, value2){
             value1.toString().localeCompare(value2.toString())
@@ -219,5 +265,5 @@ return function compareAlphabetical(value1, value2) {
     return convertStringToAlphaNumerical(value1).localeCompare(
     convertStringToAlphaNumerical(value2)
     )
-}
+} */
 }
