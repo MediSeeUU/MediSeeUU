5cce902511e60fed5cd0d6f567849cfa5f23fd44
Rutger <rutgerdj2@gmail.com>
2022-04-13 14:13:27 +0200

    [BACKEND] Remove duplicate model files

--- medctrl-backend/API/api/models/procedure_type.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ProcedureType(models.Model):
-    procedure_type_id = models.UUIDField(primary_key=True)
-    description = models.CharField(max_length=320)
-
-    class Meta:
-        db_table = "procedure_type"
--- medctrl-backend/API/api/models/region.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Region(models.Model):
-    region_id = models.UUIDField(primary_key=True)
-    description = models.CharField(max_length=320)
-
-    class Meta:
-        db_table = "region"
