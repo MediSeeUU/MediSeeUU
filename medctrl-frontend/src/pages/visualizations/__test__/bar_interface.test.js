d2efe41b2d0d79efc8bd427f4f43a9d883e23be9
Aron-VH <aronvh6@gmail.com>
2022-04-14 14:18:55 +0200

    [FRONTEND] start of test of bar interface

---
+++ medctrl-frontend/src/pages/visualizations/__test__/bar_interface.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  cleanup,
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+  getByRole,
+  getByText,
+} from '@testing-library/react'
+import GenerateBarSeries from '../single_visualization/data_interfaces/BarInterface'
+
+test('no selected categories', () => {
+	//const uniqueCategories = 
+  //GenerateBarSeries({xAxis: 'DecisionYear', yAxis: 'Rapporteur', })	
+})
