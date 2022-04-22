0cc995b6dd66dfb9d378321acc621068cd5ce3ab
Rutger <rutgerdj2@gmail.com>
2022-04-22 13:32:54 +0200

    [BACKEND] Automatically create foreign key objects if needed
    Added a `Widget` so Django can automatically create objects that are a foreign key for `Medicine` when importing a file

--- medctrl-backend/API/api/admin.py
+++ medctrl-backend/API/api/admin.py
@@ -2,19 +2,57 @@
 
 # Register your models here.
 
-from import_export.admin import ImportExportModelAdmin
-from import_export import resources
 from django.contrib import admin
-from .models.medicine_models.medicine import Medicine
+from import_export import fields, resources, widgets, admin as import_admin
+
+from api.models.medicine_models.medicine import Medicine, Lookupatccode
+from api.models.medicine_models import Lookuplegalbasis, Lookuplegalscope
+
+
+def import_foreign_key(field, model):
+    """
+    Create a ForeignKey field for a given field and model.
+    Can be used to automatically create foreign key fields for import if they don't exist already.
+
+    :param field: The field to create a ForeignKey for
+    :param model: The model to create a ForeignKey for
+    """
+    return fields.Field(
+        column_name=field, attribute=field, widget=CustomForeignKeyWidget(model, field)
+    )
+
+
+class CustomForeignKeyWidget(widgets.ForeignKeyWidget):
+    """
+    Custom ForeignKeyWidget that creates a new object if it doesn't exist already.
+    """
+
+    def clean(self, value, row, *args, **kwargs):
+        res, _ = self.model.objects.get_or_create(**{self.field: value})
+
+        return res
 
 
 class MedicineResource(resources.ModelResource):
+    """
+    Resource for the Medicine model.
+    Has explicit foreign keys so Django import/export can automatically create the values if needed.
+    """
+
+    atccode = import_foreign_key("atccode", Lookupatccode)
+    legalbasis = import_foreign_key("legalbasis", Lookuplegalbasis)
+    legalscope = import_foreign_key("legalscope", Lookuplegalscope)
+
     class Meta:
+        """
+        Meta class for MedicineResource
+        """
+
         model = Medicine
         import_id_fields = ("eunumber",)
 
 
-class MedicineAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+class MedicineAdmin(import_admin.ImportExportModelAdmin, admin.ModelAdmin):
     """
     Admin View for Medicine
     """
