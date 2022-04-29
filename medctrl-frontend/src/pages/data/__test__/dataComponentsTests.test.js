d4734c66e3738db24eebca19951357d0a6fd017f
ManoukSirag <Siragmanouk@gmail.com>
2022-04-29 16:43:35 +0200

    [FRONTEND] datacomp test prettier
    ran prettier on the edited files.

--- medctrl-frontend/src/pages/data/__test__/dataComponentsTests.test.js
+++ medctrl-frontend/src/pages/data/__test__/dataComponentsTests.test.js
@@ -13,78 +13,81 @@ import {
 import DataPage from '../DataPage.js'
 import DataSelect from '../dataComponents/DataSelect'
 import SelectedData from '../dataComponents/SelectedData'
-import { 
+import {
   DataProvider,
   SelectedContext,
   DataContext,
   ColumnSelectionContext,
   ColumnSelectionContextUpdate,
   CheckedContext,
-  CheckedContextUpdate
+  CheckedContextUpdate,
 } from '../../../shared/contexts/DataContext'
 import { BrowserRouter } from 'react-router-dom'
 import allData from '../../../testJson/data.json'
 import { check } from 'prettier'
 
-
 test('DataPage renders without crashing', () => {
   const root = document.createElement('div')
   ReactDOM.render(
-    <BrowserRouter> 
-      <DataProvider> 
-        <DataPage /> 
-      </DataProvider> 
-    </BrowserRouter>
-  , root)
+    <BrowserRouter>
+      <DataProvider>
+        <DataPage />
+      </DataProvider>
+    </BrowserRouter>,
+    root
+  )
 })
 
 test('DataSelect renders without crashing', () => {
-  const root = document.createElement('div');
+  const root = document.createElement('div')
   ReactDOM.render(
-    <BrowserRouter> 
-      <DataProvider> 
-        <DataSelect /> 
-      </DataProvider> 
-    </BrowserRouter> , root)
+    <BrowserRouter>
+      <DataProvider>
+        <DataSelect />
+      </DataProvider>
+    </BrowserRouter>,
+    root
+  )
 })
 
 test('SelectedData renders without crashing', () => {
-  const root = document.createElement('div');
-  ReactDOM.render(<SelectedData /> , root)
+  const root = document.createElement('div')
+  ReactDOM.render(<SelectedData />, root)
 })
 
 test('data can be selected', () => {
-  
-    // Render datapage with context
-    const root = document.createElement('div');
-    render(
-      <BrowserRouter> 
-        <DataProvider> 
-          <DataPage /> 
-        </DataProvider> 
-      </BrowserRouter>
-    , root)
-    
-    // Get main table
-    const table = screen.getAllByRole('table')[0]
+  // Render datapage with context
+  const root = document.createElement('div')
+  render(
+    <BrowserRouter>
+      <DataProvider>
+        <DataPage />
+      </DataProvider>
+    </BrowserRouter>,
+    root
+  )
 
-    // Get the content if the first column of the first row
-    const firstRow = screen.getAllByRole('row')[1]
-    console.log(firstRow.children[1].textContent)
+  // Get main table
+  const table = screen.getAllByRole('table')[0]
 
-    const checkBox = within(table).getAllByRole('checkbox')[1]
-    fireEvent.click(checkBox)
-    
-    // There should now be 2 tables.
-    const tables = screen.getAllByRole('table')
-    expect(tables.length === 2)
+  // Get the content if the first column of the first row
+  const firstRow = screen.getAllByRole('row')[1]
+  console.log(firstRow.children[1].textContent)
 
-    const rows = screen.getAllByRole('row')
-    const lastRow = rows[rows.length - 1]
+  // select the first entry in the main table
+  const checkBox = within(table).getAllByRole('checkbox')[1]
+  fireEvent.click(checkBox)
 
-    expect(firstRow.textContent === lastRow.textContent)
+  // There should now be 2 tables.
+  const tables = screen.getAllByRole('table')
+  expect(tables.length === 2)
 
-    
+  // get the final row (AKA the row in the second table)
+  const rows = screen.getAllByRole('row')
+  const lastRow = rows[rows.length - 1]
+
+  // the first row (selected entry) and last row (displayed entry) should have the same content
+  expect(firstRow.textContent === lastRow.textContent)
 })
 
 test('table updated', () => {
@@ -113,8 +116,7 @@ test('table updated', () => {
     columnSelection = newColumns
   }
 
-
-  const root = document.createElement('div');
+  const root = document.createElement('div')
   render(
     <BrowserRouter>
       <DataContext.Provider value={allData}>
@@ -128,8 +130,9 @@ test('table updated', () => {
           </ColumnSelectionContextUpdate.Provider>
         </ColumnSelectionContext.Provider>
       </DataContext.Provider>
-    </BrowserRouter>
-  , root)
+    </BrowserRouter>,
+    root
+  )
 
   // Open the filter & sort menu and apply a filter
   fireEvent.click(screen.getByText(/Filter & Sort/i))
@@ -146,13 +149,12 @@ test('table updated', () => {
   fireEvent.click(selectAll)
 
   // Filter all datapoints to only the selected datapoints
-  var updatedData = allData.filter(entry => checkedState[entry.EUNumber])
-  
+  var updatedData = allData.filter((entry) => checkedState[entry.EUNumber])
+
   // Check if all collected datapoints abide by the added filter
   updatedData.forEach((element) => {
     expect(element.ApplicationNo.toString()).toContain('8')
   })
-
 })
 
 test('available pages lower than current loaded', () => {
@@ -181,8 +183,7 @@ test('available pages lower than current loaded', () => {
     columnSelection = newColumns
   }
 
-
-  const root = document.createElement('div');
+  const root = document.createElement('div')
   render(
     <BrowserRouter>
       <DataContext.Provider value={allData}>
@@ -196,24 +197,25 @@ test('available pages lower than current loaded', () => {
           </ColumnSelectionContextUpdate.Provider>
         </ColumnSelectionContext.Provider>
       </DataContext.Provider>
-    </BrowserRouter>
-  , root)
+    </BrowserRouter>,
+    root
+  )
 
   // Move to a higher page number
-  const nextPage = screen.getByTestId("next-page-table")
+  const nextPage = screen.getByTestId('next-page-table')
 
-  for (let i=8; i>=0; i--) {
+  for (let i = 8; i >= 0; i--) {
     fireEvent.click(nextPage)
   }
-  
+
   // Change the amount of entries per page to 300, so that the current page number no longer exists
   const resPerPage = screen.getByDisplayValue('25')
-  fireEvent.change(resPerPage, {target : {value : 300}})
+  fireEvent.change(resPerPage, { target: { value: 300 } })
 })
 
 test('amount of pages is 3', () => {
   //Setup to render the page
-  const data = allData.slice(0,70)
+  const data = allData.slice(0, 70)
 
   let checkedState = Object.assign(
     {},
@@ -237,8 +239,7 @@ test('amount of pages is 3', () => {
     columnSelection = newColumns
   }
 
-
-  const root = document.createElement('div');
+  const root = document.createElement('div')
   render(
     <BrowserRouter>
       <DataContext.Provider value={data}>
@@ -252,77 +253,76 @@ test('amount of pages is 3', () => {
           </ColumnSelectionContextUpdate.Provider>
         </ColumnSelectionContext.Provider>
       </DataContext.Provider>
-    </BrowserRouter>
-  , root)
+    </BrowserRouter>,
+    root
+  )
 
-  // There should now be only 3 page options. 3 page options and 2 arrow buttons means the 
+  // There should now be only 3 page options. 3 page options and 2 arrow buttons means the
   // pagination div should now have 5 childnodes.
-  const paginationDiv = screen.getByTestId("pagination-div")
+  const paginationDiv = screen.getByTestId('pagination-div')
 
   expect(paginationDiv.childNodes.length == 5)
-
 })
 
 test('Can go a page forward and backwards', () => {
-    //Setup to render the page
-    const data = allData
+  //Setup to render the page
+  const data = allData
 
-    let checkedState = Object.assign(
-      {},
-      ...data.map((entry) => ({ [entry.EUNumber]: false }))
-    )
-    const setCheckedState = (newState) => {
-      checkedState = newState
-    }
-  
-    var columnSelection = [
-      'EUNoShort',
-      'BrandName',
-      'MAH',
-      'DecisionDate',
-      'ATCNameL2',
-      'ApplicationNo',
-      'ApplicationNo',
-    ]
-  
-    const setColumnSelection = (newColumns) => {
-      columnSelection = newColumns
-    }
-  
-  
-    const root = document.createElement('div');
-    render(
-      <BrowserRouter>
-        <DataContext.Provider value={allData}>
-          <ColumnSelectionContext.Provider value={columnSelection}>
-            <ColumnSelectionContextUpdate.Provider value={setColumnSelection}>
-              <CheckedContext.Provider value={checkedState}>
-                <CheckedContextUpdate.Provider value={setCheckedState}>
-                  <DataPage />
-                </CheckedContextUpdate.Provider>
-              </CheckedContext.Provider>
-            </ColumnSelectionContextUpdate.Provider>
-          </ColumnSelectionContext.Provider>
-        </DataContext.Provider>
-      </BrowserRouter>
-    , root)
+  let checkedState = Object.assign(
+    {},
+    ...data.map((entry) => ({ [entry.EUNumber]: false }))
+  )
+  const setCheckedState = (newState) => {
+    checkedState = newState
+  }
 
-    const nextPage = screen.getByTestId("next-page-table")
-    const prevPage = screen.getByTestId("prev-page-table")
+  var columnSelection = [
+    'EUNoShort',
+    'BrandName',
+    'MAH',
+    'DecisionDate',
+    'ATCNameL2',
+    'ApplicationNo',
+    'ApplicationNo',
+  ]
 
-    // Going forward 5 pages
-    for (let i=5; i>=0; i--) {
-      fireEvent.click(nextPage)
-    }
+  const setColumnSelection = (newColumns) => {
+    columnSelection = newColumns
+  }
 
-    const paginationDiv = screen.getByTestId("pagination-div")
+  const root = document.createElement('div')
+  render(
+    <BrowserRouter>
+      <DataContext.Provider value={allData}>
+        <ColumnSelectionContext.Provider value={columnSelection}>
+          <ColumnSelectionContextUpdate.Provider value={setColumnSelection}>
+            <CheckedContext.Provider value={checkedState}>
+              <CheckedContextUpdate.Provider value={setCheckedState}>
+                <DataPage />
+              </CheckedContextUpdate.Provider>
+            </CheckedContext.Provider>
+          </ColumnSelectionContextUpdate.Provider>
+        </ColumnSelectionContext.Provider>
+      </DataContext.Provider>
+    </BrowserRouter>,
+    root
+  )
 
-    // After 5 clicks, the current page should be 7
-    expect(paginationDiv.childNodes[4].textContent == '7')
+  const nextPage = screen.getByTestId('next-page-table')
+  const prevPage = screen.getByTestId('prev-page-table')
 
-    fireEvent.click(prevPage)
+  // Going forward 5 pages
+  for (let i = 5; i >= 0; i--) {
+    fireEvent.click(nextPage)
+  }
 
-    // clicking the previous page button once should bring the current page to 6
-    expect(paginationDiv.childNodes[4].textContent == '6')
+  const paginationDiv = screen.getByTestId('pagination-div')
 
+  // After 5 clicks, the current page should be 7
+  expect(paginationDiv.childNodes[4].textContent == '7')
+
+  fireEvent.click(prevPage)
+
+  // clicking the previous page button once should bring the current page to 6
+  expect(paginationDiv.childNodes[4].textContent == '6')
 })
