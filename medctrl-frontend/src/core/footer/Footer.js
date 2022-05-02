98c32d76cc7625851c67aa08e752baf8b417983e
Marco Delsink <83978789+marcodelsink@users.noreply.github.com>
2022-05-02 12:24:44 +0200

    Added hyperlink to footer and American spelling

--- medctrl-frontend/src/core/footer/Footer.js
+++ medctrl-frontend/src/core/footer/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     //Footer components, gives the information to be placed in the footer in two sections placed
     //next to each other.
     <footer className="med_footer">
-      <section id="footer__organisations">
-        <h2>Relevant organisations</h2>
+      <section id="footer__organizations">
+        <h2>Relevant organizations</h2>
 
-        <ul className="footer__organisation__list">
+        <ul className="footer__organization__list">
           <li>
             {' '}
             <a
@@ -33,7 +33,11 @@ function Footer() {
         <p>
           This is a development version of the dashboard created for the
           Medicines Evaluation Board and Utrecht University. For more
-          information please visit the home page.
+          information please visit the{' '}
+          <a className="footer__list__link" href="/info">
+            info page
+          </a>
+          .
         </p>
       </section>
     </footer>
