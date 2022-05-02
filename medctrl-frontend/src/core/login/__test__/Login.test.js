b2419473c20a21a1b20cbc7eaecc88fb0bbcd454
A. Hartog <kiwisco@hotmail.com>
2022-05-02 10:02:27 +0200

    [FRONTEND]: prettier

--- medctrl-frontend/src/core/login/__test__/Login.test.js
+++ medctrl-frontend/src/core/login/__test__/Login.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import SideNavigation from '../../navigation/Navigation'
 import { BrowserRouter } from 'react-router-dom'
 
--- medctrl-frontend/src/pages/detailed-info/__test__/DetailedInfoPage.test.js
+++ medctrl-frontend/src/pages/detailed-info/__test__/DetailedInfoPage.test.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { render,  screen } from '@testing-library/react'
-import { InfoPage, } from '../DetailedInfoPage'
+import { render, screen } from '@testing-library/react'
+import { InfoPage } from '../DetailedInfoPage'
 import allData from '../../../testJson/data.json'
 import medDataWithProcedures from '../../../../src/pages/detailed-info/detailed-info-data.json'
-
 
 test('render detailedinfopage and display correct data', () => {
   render(<InfoPage data={allData} medIDnumber={'1528'} />)
