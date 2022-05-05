8069ce03f3218512dc64198b0e6d05993e3e9cad
sfkelder <58032481+sfkelder@users.noreply.github.com>
2022-05-05 20:25:49 +0200

    Update public_Access.py

--- medctrl-backend/API/api/views/access_level_views/public_Access.py
+++ medctrl-backend/API/api/views/access_level_views/public_Access.py
@@ -1,13 +1,3 @@
-
-# from api.models.medicine_models.medicine import Medicine
-# from rest_framework.response import Response
-
-# def publicAccess(request):
-#     columns = Medicine.objects.all()
-#     print("OUTPUT...........................................................................................", columns)
-#     print(columns[0].eunumber)
-#     return Response() 
-
 from rest_framework import viewsets
 from api.serializers.access_level_serializers import MedicineSerializer
 from api.models.medicine_models import Medicine
