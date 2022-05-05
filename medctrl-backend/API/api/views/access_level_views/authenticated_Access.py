1ba15d29cd042ee853b9b18cf777a150d7137c06
sfkelder <58032481+sfkelder@users.noreply.github.com>
2022-05-05 21:10:46 +0200

    Update authenticated_Access.py

--- medctrl-backend/API/api/views/access_level_views/authenticated_Access.py
+++ medctrl-backend/API/api/views/access_level_views/authenticated_Access.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
 from rest_framework.response import Response
-#from api..serializers import UserSerializer
+from api.serializers.user_serializers import UserSerializer
 
 class AuthenticatedUser(generics.RetrieveAPIView):
-  permission_classes = [permissions.IsAuthenticated]
+  permission_classes = [IsAuthenticated]
   serializer_class = UserSerializer
 
   def get_object(self):
