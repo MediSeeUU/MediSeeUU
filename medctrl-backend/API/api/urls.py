fef869909b3ad5946e4ac34f1467423e6c702da0
Siebe Vuijst <s.vuijst@students.uu.nl>
2022-05-06 10:50:32 +0200

    [BACKEND] fixed errors

--- medctrl-backend/API/api/urls.py
+++ medctrl-backend/API/api/urls.py
@@ -2,14 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from knox import views as knox_views
 
-from api.views.medicine_views import ProcedureViewSet, MedicineViewSet
+from api.views.medicine_views import ProcedureViewSet, MedicineViewSet, AuthenticatedUser
 from api.views.account_views import RegisterAPI, LoginAPI
-from api.views.access_level_views import PublicAccessViewSet, AuthenticatedUser
 
 router = DefaultRouter()
 router.register(r"procedure", ProcedureViewSet, basename="procedure")
 router.register(r"medicine", MedicineViewSet , basename="medicine")
-router.register(r"authenticated", AuthenticatedUser , basename="authenticatedUser")
+# router.register(r"authenticated", AuthenticatedUser , basename="authenticatedUser") this is giving me errors
 
 urlpatterns = [
     path("", include(router.urls)),
--- medctrl-backend/API/api/views/medicine_views/authenticated_Access.py
+++ medctrl-backend/API/api/views/medicine_views/authenticated_Access.py
@@ -1,9 +1,9 @@
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from rest_framework.response import Response
 from api.serializers.user_serializers import UserSerializer
 
 class AuthenticatedUser(generics.RetrieveAPIView):
-  permission_classes = [IsAuthenticated]
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = UserSerializer
 
   def get_object(self):
