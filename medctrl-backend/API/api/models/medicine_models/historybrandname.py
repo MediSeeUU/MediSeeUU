5f66dbb490dc071eb5b02826896486549e8234c2
Asehujiko <Asehujiko_@hotmail.com>
2022-05-10 10:35:47 +0200

    [backend] support for longer name
    changed the django default character limits to accomodate some long-named outliers in the brand names and market authorisation holders

--- medctrl-backend/API/api/models/medicine_models/historybrandname.py
+++ medctrl-backend/API/api/models/medicine_models/historybrandname.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Historybrandname(models.Model):
     eunumber = models.ForeignKey("Medicine", models.CASCADE, db_column="EUNumber")
-    brandname = models.CharField(db_column="BrandName", max_length=45)
+    brandname = models.CharField(db_column="BrandName", max_length=255)
     brandnamedate = models.DateField(db_column="BrandNameDate", blank=True, null=True)
 
     class Meta:
