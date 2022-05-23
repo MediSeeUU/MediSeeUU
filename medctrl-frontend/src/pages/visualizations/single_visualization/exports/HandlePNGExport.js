ca6308086ab7b3db587936da93d719f748a26cfd
Aron-VH <aronvh6@gmail.com>
2022-05-23 16:11:55 +0200

    [FRONTEND] fixed the png export

--- medctrl-frontend/src/pages/visualizations/single_visualization/exports/HandlePNGExport.js
+++ medctrl-frontend/src/pages/visualizations/single_visualization/exports/HandlePNGExport.js
@@ -4,21 +4,16 @@ import { changeDpiDataUrl } from 'changedpi'
 // Event handler for exporting the visualization to png.
 // Does not export the actual data.
 export default function HandlePNGExport(id, title, ApexCharts) {
-  // Get the visualization in the base64 format,
-  const imgURI = ApexCharts.exec(String(id), 'dataURI', {
-    scale: 3.5,
-  }) /* .catch(err =>
-      console.log('an error occurred when trying to export to png: {' + err + '}')
-    ) */
-  // changes the dpi of the visualization to 300
-  const dataURI300 = changeDpiDataUrl(imgURI, 300)
   const fileName = 'Graph ' + id + ' - ' + title
   const fileType = '.png'
-
   const downloadLink = document.createElement('a')
-  downloadLink.href = dataURI300
-  downloadLink.download = fileName + fileType
-  document.body.appendChild(downloadLink)
-  downloadLink.click()
-  document.body.removeChild(downloadLink)
+  // Get the visualization in the base64 format
+  ApexCharts.exec(String(id), 'dataURI', { scale: 3.5 }).then(({ imgURI }) => {
+    // changes the dpi of the visualization to 300
+    downloadLink.href = changeDpiDataUrl(imgURI, 300)
+    downloadLink.download = fileName + fileType
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+  })
 }
