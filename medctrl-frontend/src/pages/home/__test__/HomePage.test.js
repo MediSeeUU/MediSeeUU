f09fb893daf30373cd39262a4187bf9827476cf1
Siebe Vuijst <s.vuijst@students.uu.nl>
2022-05-30 17:40:06 +0200

    [FRONTEND] Fix failing unit test

--- medctrl-frontend/src/pages/home/__test__/HomePage.test.js
+++ medctrl-frontend/src/pages/home/__test__/HomePage.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import HomePage from '../HomePage'
 import { fireEvent, screen, render } from '@testing-library/react'
+import { TableUtilsContext, TableUtilsUpdateContext } from '../../../shared/contexts/DataContext'
 
 test('homepage renders without crashing', () => {
   const root = document.createElement('div')
@@ -15,10 +16,19 @@ test('homepage renders without crashing', () => {
 })
 
 test('homepage search bar responds correctly', () => {
+  const update = (utils) => {
+    expect(utils.search).toStrictEqual(new String('pfizer'))
+  }
   render(
     <BrowserRouter>
-      <HomePage />
+      <TableUtilsContext.Provider value={{search: ''}}>
+        <TableUtilsUpdateContext.Provider value={update}>
+          <HomePage />
+        </TableUtilsUpdateContext.Provider>
+      </TableUtilsContext.Provider>
     </BrowserRouter>
   )
+  const input = screen.getByRole('textbox')
+  fireEvent.change(input, { target: { value: 'pfizer' } })
   fireEvent.click(screen.getByText('Search'))
 })
