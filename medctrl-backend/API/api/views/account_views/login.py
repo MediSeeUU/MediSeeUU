0cbfab9873b9a88762ad681ee8d86554dd49382a
sfkelder <58032481+sfkelder@users.noreply.github.com>
2022-06-07 10:08:19 +0200

    Update login.py
    extended comments

--- medctrl-backend/API/api/views/account_views/login.py
+++ medctrl-backend/API/api/views/account_views/login.py
@@ -1,9 +1,14 @@
+# This file contains all the logic for signing in on the webpage
+#------------------------------------------------------------------
+
 from django.contrib.auth import login
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 
-#Checks wether a user has valid login credenials using the knox authentication framework
+# Checks wether a user has valid login credenials using the knox authentication framework
+# The 'login' function is a default funcion in the django framework used for validating 
+# an user and creating a token.
 class LoginAPI(KnoxLoginView):
     """Login API View"""
 
@@ -14,5 +19,5 @@ class LoginAPI(KnoxLoginView):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
-        login(request, user)
+        login(request, user) 
         return super().post(request, format=_format)
