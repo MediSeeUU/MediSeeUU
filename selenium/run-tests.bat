cddda432665e6c0172446c293ded6bd74d0c51eb
Marco Delsink <83978789+marcodelsink@users.noreply.github.com>
2022-06-08 12:25:54 +0200

    Remove copyright statements from Selenium

--- selenium/run-tests.bat
+++ selenium/run-tests.bat
@@ -1,6 +1,3 @@
-@REM This program has been developed by students from the bachelor Computer Science at
-@REM Utrecht University within the Software Project course.
-@REM © Copyright Utrecht University (Department of Information and Computing Sciences)
 @echo off
 CALL venv/scripts/activate.bat
 py TestRunner.py
