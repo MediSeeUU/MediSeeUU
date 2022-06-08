8cff1ac01d413454a2d05df7d65230ea033c5aae
Marco Delsink <83978789+marcodelsink@users.noreply.github.com>
2022-06-08 12:28:10 +0200

    Revert "Remove copyright statements from Selenium"
    This reverts commit cddda432665e6c0172446c293ded6bd74d0c51eb.

--- selenium/Pages/home_page.py
+++ selenium/Pages/home_page.py
@@ -1,3 +1,6 @@
+# This program has been developed by students from the bachelor Computer Science at
+# Utrecht University within the Software Project course.
+# © Copyright Utrecht University (Department of Information and Computing Sciences)
 from Pages.base_page import BasePage
 from Pages.shared.search import Search
 from Pages.shared.table import Table
