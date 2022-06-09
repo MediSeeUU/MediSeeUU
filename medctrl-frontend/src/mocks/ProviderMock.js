b9dad2e75795c06b9dd3d25e032550a853769058
Siebe Vuijst <s.vuijst@students.uu.nl>
2022-06-09 13:59:37 +0200

    [FRONTEND] Added comments to mocks

--- medctrl-frontend/src/mocks/ProviderMock.js
+++ medctrl-frontend/src/mocks/ProviderMock.js
@@ -1,6 +1,8 @@
 import Provider from '../shared/Provider'
 import mockObject from './mockObject'
 
+// Function based component that mocks the provider
+// This is used to be able to unit test components that need certain data
 function MockProvider({ mock, children }) {
   return <Provider mock={mock || mockObject}>{children}</Provider>
 }
