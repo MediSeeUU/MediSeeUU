9b03a3928592facd7da574d6d33d5d338a6c65b0
Aron-VH <aronvh6@gmail.com>
2022-06-10 14:21:07 +0200

    [FRONTEND] small changes to comments for consistency

--- medctrl-frontend/src/pages/visualizations/single_visualization/exports/HandlePNGExport.js
+++ medctrl-frontend/src/pages/visualizations/single_visualization/exports/HandlePNGExport.js
@@ -1,13 +1,12 @@
 // external imports
 import { changeDpiDataUrl } from 'changedpi'
 
-// Event handler for exporting the visualization to png.
-// Does not export the actual data.
+// event handler for exporting the visualization to png
 export default function HandlePNGExport(id, title, ApexCharts) {
   const fileName = 'Graph ' + id + ' - ' + title
   const fileType = '.png'
   const downloadLink = document.createElement('a')
-  // Get the visualization in the base64 format
+  // get the visualization in the base64 format
   ApexCharts.exec(String(id), 'dataURI', { scale: 3.5 }).then(({ imgURI }) => {
     // changes the dpi of the visualization to 300
     downloadLink.href = changeDpiDataUrl(imgURI, 300)
