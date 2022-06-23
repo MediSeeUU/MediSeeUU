980fc039a00df0db99926b53c61d2e69012b11d0
sfkelder <58032481+sfkelder@users.noreply.github.com>
2022-06-23 12:56:10 +0200

    Update historyauthorisation.py

--- medctrl-backend/API/api/models/medicine_models/historyauthorisation.py
+++ medctrl-backend/API/api/models/medicine_models/historyauthorisation.py
@@ -7,8 +7,6 @@ from django.db import models
 class Historyauthorisation(models.Model):
     """
     Model class for the authorisation history table.
-    As the abstract functionality for all model classes is nearly identical,
-    information about them has been moved to an external readme.
     """
 
     eunumber = models.ForeignKey("Medicine", models.CASCADE, db_column="EUNumber")
