465af197e42a586582dc8a3a8cd377d092ab1546
Yfke Smit <smityfke@gmail.com>
2022-09-29 15:31:15 +0200

    Refactor webscraper main and ec_scraper to PEP8

--- medctrl-scraper/webscraper/src/__main__.py
+++ medctrl-scraper/webscraper/src/__main__.py
@@ -1,20 +1,21 @@
 # Entry point of the Python code
 # This will run if you run `python3 .` in the directory
 
-from pathlib import Path
-from joblib import Parallel, delayed
-from time import sleep
-import pandas as pd
 import csv
-import math
+from pathlib import Path
 
-# Create the the data dir. 
+import pandas as pd
+from joblib import Parallel, delayed
+
+import ec_scraper
+
+# Create the data dir.
 # The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
 path_auth_descis = Path("./data/authorisation_decisions")
-path_smpcs       = Path("./data/smpcs")
-path_epars       = Path("./data/epars")
-path_annexes     = Path("./data/annexes")
-path_csv         = Path("./data/CSV")
+path_smpcs = Path("./data/smpcs")
+path_epars = Path("./data/epars")
+path_annexes = Path("./data/annexes")
+path_csv = Path("./data/CSV")
 
 path_auth_descis.mkdir(parents=True, exist_ok=True)
 path_smpcs.mkdir(exist_ok=True)
@@ -22,24 +23,25 @@ path_epars.mkdir(exist_ok=True)
 path_annexes.mkdir(exist_ok=True)
 path_csv.mkdir(exist_ok=True)
 
-import ec_scraper
+medicine_codes = ec_scraper.scrape_medicine_urls(
+    "https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm")
 
-medicine_codes = ec_scraper.scrape_medicine_urls("https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm")
 
-#Parallelised function for getting the URL codes. They are written to a CSV file
-def getURLs(medicine):
+# Paralleled function for getting the URL codes. They are written to a CSV file
+def get_urls(medicine):
     attempts = 0
     max_attempts = 4
     success = False
     while attempts < max_attempts and not success:
         try:
-            #getURLsForPDFAndEMA returns per medicine the urls for the decision and annexes files and for the ema website.
-            dec_list, anx_list, ema_list = ec_scraper.getURLsForPDFAndEMA(medicine)
+            # getURLsForPDFAndEMA returns per medicine the urls for the decision and annexes files and for the ema
+            # website.
+            dec_list, anx_list, ema_list = ec_scraper.get_urls_for_pdf_and_ema(medicine)
             with open("./data/CSV/decision.csv", 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow([medicine, dec_list])
             with open("./data/CSV/annexes.csv", 'a') as f:
-                writer = csv.writer(f)                    
+                writer = csv.writer(f)
                 writer.writerow([medicine, anx_list])
             with open("./data/CSV/ema_urls.csv", 'a') as f:
                 writer = csv.writer(f)
@@ -50,39 +52,44 @@ def getURLs(medicine):
             if attempts == max_attempts:
                 break
 
-#NOTE: Use the line of code below to fill all the CSV files. If you have complete CSV file, this line of code is not needed.
-#Parallel(n_jobs=12)(delayed(getURLs)(medicine) for medicine in medicine_codes)
+
+# NOTE: Use the line of code below to fill all the CSV files.
+# If you have complete CSV file, this line of code below is not needed.
+# Parallel(n_jobs=12)(delayed(getURLs)(medicine) for medicine in medicine_codes)
 print("Done with URL getting")
 
-#Download pdfs using the dictionaries created from the CSV files
-def downloadPDFs(med_id, type, dict):
+
+# Download pdfs using the dictionaries created from the CSV files
+def download_pdfs(med_id, pdf_type, type_dict):
     attempts = 0
     max_attempts = 4
     success = False
     while attempts < max_attempts and not success:
         try:
-            for url in list(dict[med_id].strip("[]").split(", ")): #Make sure the input from the CSV are passed on correctly to the download function
-                ec_scraper.download_PDF_from_URL(med_id, url[1:-1], type)
+            for url in list(type_dict[med_id].strip("[]").split(", ")):  # parse csv input
+                print(url[1:-1])
+                ec_scraper.download_pdf_from_url(med_id, url[1:-1], pdf_type)
             success = True
         except:
             attempts += 1
             if attempts == max_attempts:
                 break
 
-#Function for reading the CSV contents back into dictionaries that can be used for downloading.
-def readCSVfiles():
-    decisions   = pd.read_csv('./data/CSV/decision.csv', header=None, index_col=0, squeeze=True).to_dict()
-    annexes     = pd.read_csv('./data/CSV/annexes.csv', header=None, index_col=0, squeeze=True).to_dict()
-    ema_urls    = pd.read_csv('./data/CSV/ema_urls.csv', header=None, index_col=0, squeeze=True).to_dict()
-    return decisions, annexes, ema_urls
 
-decisions, annexes, ema_urls = readCSVfiles()
+# Function for reading the CSV contents back into dictionaries that can be used for downloading.
+def read_csv_files():
+    dec = pd.read_csv('./data/CSV/decision.csv', header=None, index_col=0, squeeze=True).to_dict()
+    anx = pd.read_csv('./data/CSV/annexes.csv', header=None, index_col=0, squeeze=True).to_dict()
+    ema = pd.read_csv('./data/CSV/ema_urls.csv', header=None, index_col=0, squeeze=True).to_dict()
+    return dec, anx, ema
 
-#Download the decision files, parallel
-Parallel(n_jobs=12)(delayed(downloadPDFs)(medicine, "dec", decisions) for medicine in decisions)
+
+# Store the result of the csv converting into dictionaries
+decisions, annexes, ema_urls = read_csv_files()
+
+# Download the decision files, parallel
+Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "dec", decisions) for medicine in decisions)
 print("Done with decisions")
-#Download the annexes files, parallel
-Parallel(n_jobs=12)(delayed(downloadPDFs)(medicine, "anx", annexes) for medicine in annexes)
+# Download the annexes files, parallel
+Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "anx", annexes) for medicine in annexes)
 print("Done with Annexes")
-
-    
--- medctrl-scraper/webscraper/src/ec_scraper.py
+++ medctrl-scraper/webscraper/src/ec_scraper.py
@@ -1,12 +1,11 @@
-from pydoc import plain
-import requests
-import bs4
-import regex as re
 import json
 import sys
-import pandas as pd
 from datetime import datetime
-from enum import Enum
+
+import bs4
+import regex as re
+import requests
+
 
 def scrape_medicine_urls(url: str):
     # Links acquired from "https://ec.europa.eu/health/documents/community-register/html/index_en.htm"
@@ -21,13 +20,13 @@ def scrape_medicine_urls(url: str):
 
     # The data we are looking for is contained in a script tag. A JSON object containing called 'dataSet'
     # We find all script tags, and isolate the one that contains the data we want.
-    # We use regex, because mathing on a string directly will only return *exact* matches.
-    scriptTag = soup.find("script", string=re.compile(r"dataSet"))
+    # We use regex, because matching on a string directly will only return *exact* matches.
+    script_tag = soup.find("script", string=re.compile(r"dataSet"))
 
     # Regex r"\[.*\]" matches '[', all items in between, and ']'
     # TODO: Check if script tags include other lists, make more robust.
-    plaintext_json: str = re.findall(r"\[.*\]", scriptTag.text, re.DOTALL)[0]
-    plaintext_json      = plaintext_json.replace("\r", "").replace("\n", "") # Remove newlines
+    plaintext_json: str = re.findall(r"\[.*\]", script_tag.text, re.DOTALL)[0]
+    plaintext_json = plaintext_json.replace("\r", "").replace("\n", "")  # Remove newlines
 
     parsed_json = json.loads(plaintext_json)
 
@@ -38,11 +37,11 @@ def scrape_medicine_urls(url: str):
     for row in parsed_json:
         string_url = row['eu_num']['pre'] + row['eu_num']['id']
         url_list.append(string_url)
-        
+
     return url_list
 
 
-def getURLsForPDFAndEMA(url: str):
+def get_urls_for_pdf_and_ema(url: str):
     html_active = requests.get(f"https://ec.europa.eu/health/documents/community-register/html/{url}.htm")
 
     # If an http error occurred, throw error
@@ -50,12 +49,12 @@ def getURLsForPDFAndEMA(url: str):
     html_active.raise_for_status()
 
     soup = bs4.BeautifulSoup(html_active.text, "html.parser")
-    
+
     # The data we are looking for is contained in a script tag. A JSON object containing called 'dataSet_proc'
     # We find all script tags, and isolate the one that contains the data we want.
     # We use regex, because matching on a string directly will only return *exact* matches.
-    scriptTag = soup.find("script", string=re.compile(r"dataSet_proc"))
-    
+    script_tag = soup.find("script", string=re.compile(r"dataSet_proc"))
+
     # NOTE:
     # The website for specific medicines contains two JSON objects in the script tag.
     # "dataSet_product_information" and "dataSet_proc"
@@ -67,70 +66,72 @@ def getURLsForPDFAndEMA(url: str):
     #   matches '[', all items in between, and ']'
     #   and if it is closed with a ;
     #   which returns the values of the two JSON objects "dataSet_product_information" and "dataSet_proc"
-    plaintext_json = re.findall(r"(?<=var .+?)\[.+?\](?=;)", scriptTag.text, re.DOTALL)
-    for match in plaintext_json: match = match.replace("\r", "").replace("\n", "") # Remove newlines
-        
-    #get the url linking to the EMA website, where the EPAR + OMAR files can be found. Sometimes there are two URLS to the EMA website.
-    ema_url_list = (re.findall(r"\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'\(\),\$_\{\}\^~\[\]`#|]+",plaintext_json[0]))
+    plaintext_json = re.findall(r"(?<=var .+?)\[.+?\](?=;)", script_tag.text, re.DOTALL)
+    for match in plaintext_json:
+        match = match.replace("\r", "").replace("\n", "")  # Remove newlines
+
+    # get the url linking to the EMA website, where the EPAR + OMAR files can be found. Sometimes there are two URLS
+    # to the EMA website.
+    ema_url_list = (re.findall(r"\w+:\/\/[-a-zA-Z0-9:@;?&=\/%\+\.\*!'\(\),\$_\{\}\^~\[\]`#|]+", plaintext_json[0]))
     procedures_json = json.loads(plaintext_json[1])
 
-    #save prints to file
+    # save prints to file
     original_stdout = sys.stdout
-    sys.stdout = open ('log.txt', 'w')
+    sys.stdout = open('log.txt', 'w')
 
     print(f"Starting with {url}")
 
-    #define lists to save the urls
+    # define lists to save the urls
     dec_url_list = []
     anx_url_list = []
 
-    #for each row in the json file of each medicine, get the urls for the pdfs of the decision and annexes.
+    # for each row in the json file of each medicine, get the urls for the pdfs of the decision and annexes.
     for row in procedures_json:
-        if row["files_dec"] == None and row["files_anx"] == None:
+        if row["files_dec"] is None and row["files_anx"] is None:
             continue
-        
+
         decision_date = datetime.strptime(row["decision"]["date"], f"%Y-%m-%d").date()
-        decision_id   = row["id"]
-    
+        decision_id = row["id"]
+
         if row["files_dec"]:
-            pdf_url_dec = f"""{decision_date.year}/{decision_date.strftime("%Y%m%d")}{decision_id}/dec_{decision_id}_en.pdf"""  
-            #add url to decision pdf to list
-            dec_url_list.append("https://ec.europa.eu/health/documents/community-register/" + pdf_url_dec)  
-            #NOTE: if you want to download the PDFs immediately while getting the URLs:
-            #downloadDecPDFfromURL(url, pdf_url_dec, decision_id)
-                
+            pdf_url_dec = f"""{decision_date.year}/{decision_date.strftime("%Y%m%d")}{decision_id}/dec_{decision_id}_en.pdf"""
+            # add url to decision pdf to list
+            dec_url_list.append("https://ec.europa.eu/health/documents/community-register/" + pdf_url_dec)
+            # NOTE: if you want to download the PDFs immediately while getting the URLs:
+            # downloadDecPDFfromURL(url, pdf_url_dec, decision_id)
+
         if row["files_anx"]:
             pdf_url_anx = f"""{decision_date.year}/{decision_date.strftime("%Y%m%d")}{decision_id}/anx_{decision_id}_en.pdf"""
-            #add url to annexes pdf to list
+            # add url to annexes pdf to list
             anx_url_list.append("https://ec.europa.eu/health/documents/community-register/" + pdf_url_anx)
-            #NOTE: if you want to download the PDFs immediately while getting the URLs:
-            #downloadAnnPDFfromURL(url, pdf_url_anx, decision_id)
-            
+            # NOTE: if you want to download the PDFs immediately while getting the URLs:
+            # downloadAnnPDFfromURL(url, pdf_url_anx, decision_id)
+
     # TODO: Proper logging here
     print(f"Finished with {url}")
-    sys.stdout = original_stdout 
-    #return the urls per medicine
-    return dec_url_list,anx_url_list,ema_url_list
+    sys.stdout = original_stdout
+    # return the urls per medicine
+    return dec_url_list, anx_url_list, ema_url_list
 
-# TODO: download PDFs from an URL. Maybe different methods for the different files? Decision/Anx and EMA pdfs
 
-# In the end the string will look like: https://ec.europa.eu/health/documents/community-register/2004/200404287648/dec_7648_en.pdf
+# In the end the string will look like:
+# https://ec.europa.eu/health/documents/community-register/2004/200404287648/dec_7648_en.pdf
 # TODO: Graceful failure on failed request
 # TODO: Retry on failed request
 
-def download_PDF_from_URL(med_id: str, url:str, type: str):
+def download_pdf_from_url(med_id: str, url: str, pdf_type: str):
     downloaded_file = requests.get(url)
-    dec_id = re.findall(rf"(?<={type}_)\d+", url)[0]
-    if type == "dec":
-        with open(f"./data/authorisation_decisions/{med_id}_dec_{dec_id}.pdf","wb") as file:
+    dec_id = re.findall(rf"(?<={pdf_type}_)\d+", url)[0]
+    if pdf_type == "dec":
+        with open(f"./data/authorisation_decisions/{med_id}_dec_{dec_id}.pdf", "wb") as file:
             file.write(downloaded_file.content)
             # print(f"  Downloaded {dec_id} decision")
-    elif type == "anx":
+    elif pdf_type == "anx":
         with open(f"./data/annexes/{med_id}_anx_{dec_id}.pdf", "wb") as file:
             file.write(downloaded_file.content)
             # print(f"  Downloaded {dec_id} annex")
 
-#NOTE: Below the old download code:               
+# NOTE: Below the old download code:
 # def downloadDecPDFfromURL(url: str, pdf_url_dec: str, decision_id):
 #     downloaded_file = requests.get("https://ec.europa.eu/health/documents/community-register/" + pdf_url_dec)
 #     with open(f"./data/authorisation_decisions/{url}_dec_{decision_id}.pdf", "wb") as file:
@@ -142,4 +143,3 @@ def download_PDF_from_URL(med_id: str, url:str, type: str):
 #     with open(f"./data/annexes/{url}_anx_{decision_id}.pdf", "wb") as file:
 #         file.write(downloaded_file.content)
 #         print(f"  Downloaded {decision_id} annex")
-
