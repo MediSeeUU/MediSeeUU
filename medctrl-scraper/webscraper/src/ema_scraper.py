e72c1f2893cf8a5444a09bd97a1e0603d328b328
Matthijs Boers <matthijsje.boers@gmail.com>
2022-10-03 17:00:22 +0200

    EMA Scraper refinement and error-throwing

--- medctrl-scraper/webscraper/src/ema_scraper.py
+++ medctrl-scraper/webscraper/src/ema_scraper.py
@@ -17,20 +17,32 @@ def pdf_links_from_url(url: str):
 
     soup = bs4.BeautifulSoup(html_obj.text, "html.parser")
 
+    # The documents we search are under the header
+    #   Initial marketing-authorisation documents
+    # Element is in a <div><h4><span>text</span></h4> [pdfs here] </div>
+    # Get parent thrice to find the tag that the PDFs are located in.
+    # Parent of the found object is considered <span>
+    specific_soup = soup.find(string="Initial marketing-authorisation documents").parent.parent.parent
+
     # Compiles a list of all link tags, that link to a .pdf file.
-    link_tags = soup.findAll('a', href=re.compile(r"en.pdf"))
+    link_tags = specific_soup.find_all('a')
 
     # Get all links to pdf files from the
-    link_list = list(map(lambda a: a["href"], link_tags))
+    link_list: [str] = list(map(lambda a: a["href"], link_tags))
 
-    # Find the assessment report
-    assessment_reports = list(filter(lambda x: "public-assessment-report" in x, link_list))
+    # TEMP: https://www.ema.europa.eu/documents/assessment-report/faslodex-epar-public-assessment-report_en.pdf
+    # TEMP: https://www.ema.europa.eu/documents/scientific-discussion/ambirix-epar-scientific-discussion_en.pdf
+    # TEMP: https://www.ema.europa.eu/documents/procedural-steps/ambirix-epar-procedural-steps-taken-authorisation_en.pdf
+    priority_list = ["public-assessment-report", "scientific-discussion", "procedural-steps-taken-authorisation"]
 
-    if len(assessment_reports) == 0:
-        print(f"No PDF for {medicine_name}")
-        return []
+    # Go through all links, try to find the document that is the highest on the priority list first.
+    for type_of_report in priority_list:
+        for item in link_list:
+            if type_of_report in item:
+                return item
 
-    print("debug stop point")
+    # If nothing is found, we throw an exception. This also passes all found URLs on newlines
+    raise Exception(f"No valid URLs found for {medicine_name}. Found URLs are listed below.\n" + "\n".join(link_list))
 
 
-pdf_links_from_url("https://www.ema.europa.eu/en/medicines/human/EPAR/abraxane")
+print(pdf_links_from_url("https://www.ema.europa.eu/en/medicines/human/EPAR/faslodex"))
