6551274831f5a8e4349a5dc5219a7a457c4203dd
Jacky <jackykessels@hotmail.com>
2022-10-11 14:56:16 +0200

    Fixing imports

--- medctrl-backend/API/api/models/medicine_models/__init__.py
+++ medctrl-backend/API/api/models/medicine_models/__init__.py
@@ -1,13 +1,12 @@
 # This program has been developed by students from the bachelor Computer Science at
 # Utrecht University within the Software Project course.
 # © Copyright Utrecht University (Department of Information and Computing Sciences)
-from .authorisation import Authorisation
+from .history_atc_code import HistoryATCCode
 from .history_authorisation_type import HistoryAuthorisationType
+from .history_brand_name import HistoryAuthorisationStatus
 from .history_brand_name import Historybrandname
 from .history_number_check import HistoryEMANumberCheck
 from .history_mah import Historymah
 from .history_od import HistoryOD
 from .history_prime import HistoryPrime
 from .medicine import Medicine
-from .medisee import Medisee
-from .procedure import Procedure
--- medctrl-backend/API/api/models/medicine_models/history_od.py
+++ medctrl-backend/API/api/models/medicine_models/history_od.py
@@ -2,7 +2,7 @@
 # Utrecht University within the Software Project course.
 # © Copyright Utrecht University (Department of Information and Computing Sciences)
 from django.db import models
-
+from .common import create_dashboard_column, Category
 
 class HistoryOD(models.Model):
     """
