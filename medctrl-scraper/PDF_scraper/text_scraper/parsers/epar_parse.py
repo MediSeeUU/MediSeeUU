29709b2f71ca6869fceeefe23da544e5c6aa9186
elioverhoef <49610595+elioverhoef@users.noreply.github.com>
2022-10-12 18:49:48 +0200

    Meer experimentatie met XML :D

--- medctrl-scraper/PDF_scraper/text_scraper/parsers/epar_parse.py
+++ medctrl-scraper/PDF_scraper/text_scraper/parsers/epar_parse.py
@@ -9,50 +9,37 @@ date_pattern: str = r'\d{1,2} \w+ \d{4}'
 procedure_info = 'information on the procedure'
 
 
-def get_default(filename):
-    default = 'Not parsed'
-    return {'filename': filename,
-            'ema_procedure_start_initial': default,
-            'chmp_opinion_date': default,
-            'eu_legal_basis': default
-            }
-
-
 def get_all(filedata, xml_data):
-    filedata['ema_procedure_start_initial'] = get_date(xml_data)
-    filedata['chmp_opinion_date'] = get_opinion_date(xml_data)
-    filedata['eu_legal_basis'] = get_legal_basis(xml_data)
+    filedata.ema_procedure_start_initial = get_date(xml_data)
+    #filedata.chmp_opinion_date = get_opinion_date(xml_data)
+    #filedata.eu_legal_basis = get_legal_basis(xml_data)
     return filedata
 
 
-def parse_file(filename, medicine_struct):
-    filedata = get_default(filename)
-    xml_tree = ET.parse(filename)
+def parse_file(file_path, medicine_struct):
+    print(file_path)
+    xml_tree = ET.parse(file_path)
     xml_root = xml_tree.getroot()
     xml_body = xml_root[1]
-    filedata = get_all(filedata, xml_body)
-    medicine_struct.epars = filedata
+    medicine_struct.epars = get_all(medicine_struct, xml_body)
     return medicine_struct
-
-
-# check if table front page is empty (indicating image/unreadable)
-def unreadable(xml):
-    return not xml['intro']['front_page']
 
 
 # ema_procedure_start_initial
 def get_date(xml):
     found = False
-    section = xpu.section_contains_head_substring("background information on the procedure", xml)
     regex_date = re.compile(date_pattern)
     regex_ema = re.compile(r'the application was received by the em\w+ on')
-    for txt in section:
-        if found and regex_date.search(txt):
-            return convert_month(re.search(date_pattern, txt)[0])
-        elif regex_ema.search(txt):
-            found = True
-            if regex_date.search(txt):
-                return convert_month(re.search(date_pattern, txt)[0])
+    for section in xml:
+        if xpu.section_contains_header_number('1', section[0]):
+            for p in section:
+                print (p.text)
+            # if found and regex_date.search(section):
+            #     return re.search(date_pattern, section)[0]
+            # elif regex_ema.search(section):
+            #     found = True
+            #     if regex_date.search(section):
+            #         return re.search(date_pattern, section)[0]
     return ''
 
 
