56108b5921c66cc1f31ca55885c02ce3bf33207a
TiKee99 <44522314+TiKee99@users.noreply.github.com>
2022-10-17 16:48:41 +0200

    just a commit

--- medctrl-scraper/webscraper/src/__main__.py
+++ medctrl-scraper/webscraper/src/__main__.py
@@ -9,17 +9,21 @@ from datetime import datetime
 
 import pandas as pd
 from joblib import Parallel, delayed
+import tqdm
 
 import download
 import ec_scraper
 import ema_scraper
+
+tqdm_format_string = "{l_bar}{bar}| {n_fmt}/{total_fmt} "
 
 # Global configuration of the log file
 logging.basicConfig(filename='webscraper.log', level=logging.INFO)
 
 # Local instance of a logger
 log = logging.getLogger(__name__)
-log.addHandler(logging.StreamHandler())
+log_handler = logging.StreamHandler()
+log.addHandler(log_handler)
 
 log.info(f"=== NEW LOG {datetime.today()} ===")
 
@@ -37,7 +41,7 @@ log.info("SUCCESS on scraping all medicine URLs of EC")
 
 
 # Paralleled function for getting the URL codes. They are written to a CSV file
-def get_urls_ec(medicine, eu_n):
+def get_urls_ec(medicine_url, eu_n):
     attempts = 0
     max_attempts = 4
     success = False
@@ -45,26 +49,28 @@ def get_urls_ec(medicine, eu_n):
         try:
             # getURLsForPDFAndEMA returns per medicine the urls for the decision and annexes files and for the ema
             # website.
-            dec_list, anx_list, ema_list, medicine_json, procedures_json = ec_scraper.scrape_medicine_page(medicine)
+            dec_list, anx_list, ema_list, medicine_json, procedures_json = ec_scraper.scrape_medicine_page(medicine_url)
             with open("../data/CSV/decision.csv", 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([eu_n, medicine, dec_list])
+                writer.writerow([eu_n, dec_list])
             with open("../data/CSV/annexes.csv", 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([eu_n, medicine, anx_list])
+                writer.writerow([eu_n, anx_list])
             with open("../data/CSV/ema_urls.csv", 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([eu_n, medicine, ema_list])
+                writer.writerow([eu_n, ema_list])
+            with open("../data/CSV/med_dict.csv", 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([eu_n, med_dict])
             success = True
         except Exception:
             attempts += 1
             if attempts == max_attempts:
-                print(f"failed dec/anx/ema url getting for {medicine, eu_n}")
+                log.error(f"failed dec/anx/ema url getting for {eu_n}")
                 break
 
 
-def get_urls_ema(key, url: str):
-    eu_n, medicine = key
+def get_urls_ema(medicine, url: str):
     attempts = 0
     max_attempts = 4
     success = False
@@ -77,13 +83,12 @@ def get_urls_ema(key, url: str):
             pdf_url = ema_scraper.pdf_links_from_url(url)
             with open("../data/CSV/epar.csv", 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([eu_n, medicine, pdf_url])
+                writer.writerow([medicine, pdf_url])
             success = True
         except Exception:
             attempts += 1
             if attempts == max_attempts:
-                log.error(f"failed ema_pdf url getting for {eu_n, url}")
-                # print(f"failed ema_pdf url getting for {medicine, url}")
+                log.error(f"failed ema_pdf url getting for {medicine, url}")
                 break
 
 
@@ -94,19 +99,40 @@ scrape_ec: bool = True
 
 # NOTE: Use the lines of code below to fill epar.csv
 # epar.csv will contain the links to the epar pdfs.
-scrape_ema: bool = False
+scrape_ema: bool = True
 
 # NOTE: Use the line of code below to download all files.
 download_files: bool = False
 
-if scrape_ec:
-    log.info("Scraping all medicines on the EC website")
-    Parallel(n_jobs=12)(delayed(get_urls_ec)(medicine, eu_n) for medicine, eu_n in medicine_codes)
+with Parallel(n_jobs=12) as parallel:
+    # TODO: Progressbar needs to be below logging output
+    #       https://stackoverflow.com/questions/6847862/how-to-change-the-format-of-logged-messages-temporarily-in-python
+    # log_handler.setFormatter(logging.Formatter("\r%(message)s"))
+    if scrape_ec:
+        log.info("Scraping all medicines on the EC website")
 
-if scrape_ema:
-    ema = pd.read_csv('../data/CSV/ema_urls.csv', header=None, index_col=(0, 1)).squeeze().to_dict()
-    Parallel(n_jobs=12)(delayed(get_urls_ema)(url[0], url[1]) for url in ema.items())
-    log.info("SUCCESS on scraping all individual medicine pages of EMA")
+        parallel(
+            delayed(get_urls_ec)(medicine_url, eu_n)
+            for (medicine_url, eu_n)
+            in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string)
+        )
+
+        log.info("FINISHED")
+
+    if scrape_ema:
+        log.info("Scraping all individual medicine pages of EMA")
+        ema = pd.read_csv('../data/CSV/ema_urls.csv', header=None, index_col=0).squeeze().to_dict()
+
+        parallel(
+            delayed(get_urls_ema)(url[0], url[1])
+            for url
+            in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
+        )
+
+    # TODO: Same TODO as above
+    # log_handler.setFormatter(logging.Formatter("%(message)s"))
+    log.info("FINISHED")
+
 
 if download_files:
     download.run_parallel()
--- medctrl-scraper/webscraper/src/download.py
+++ medctrl-scraper/webscraper/src/download.py
@@ -2,6 +2,7 @@ import pandas as pd
 from joblib import Parallel, delayed
 import requests
 import regex as re
+import ast
 from pathlib import Path
 
 import sys
@@ -15,54 +16,57 @@ log = logging.getLogger(__name__)
 
 def download_pdf_from_url(url: str, eu_num: str, filename_elements: list[str]):
     downloaded_file = requests.get(url)
-    filename: str = '_'.join(filename_elements) + ".pdf"
+    filename: str = f"{eu_num}_{'_'.join(filename_elements)}.pdf"
 
     # TODO: Runs this check for every downloaded file. Could be more efficient?
     path_medicine = Path(f"../data/medicines/{eu_num}")
     path_medicine.mkdir(exist_ok=True)
-
-    with open(f"../data/medicines/{eu_num}/{eu_num}_{filename}") as file:
+    with open(f"../data/medicines/{eu_num}/{filename}", "wb") as file:
         file.write(downloaded_file.content)
         log.debug(f"DOWNLOADED {filename} for {eu_num}")
 
 
 # Download pdfs using the dictionaries created from the CSV files
-def download_pdfs(med_id: str, pdf_type: str, type_dict: str):
+def download_pdfs_ec(eu_num: str, pdf_type: str, pdf_type_dict: dict(), med_dict: dict()):
     attempts = 0
     max_attempts = 4
     success = False
     # TODO: Rework with new download method
-    """
+    # TODO: Add file_number and pdf_type to filenames list.
     while attempts < max_attempts and not success:
         try:
-            for url in list(type_dict[med_id].strip("[]").split(", ")):  # parse csv input
-                if pdf_type == "epar":
-                    download_epar_from_ema(med_id, url)
-                else:
-                    download_pdf_from_ec(url[1:-1], med_id, pdf_type)
+            file_counter = 0
+            for url in ast.literal_eval(pdf_type_dict[eu_num]):
+                filename_elements = [med_dict["orphan_status"], med_dict["status_type"], pdf_type, str(file_counter)]
+                download_pdf_from_url(url, eu_num, filename_elements)
+                file_counter += 1
             success = True
         except Exception:
             attempts += 1
-    """
 
 
 # Function for reading the CSV contents back into dictionaries that can be used for downloading.
 def read_csv_files():
-    dec = pd.read_csv('../data/CSV/decision.csv', header=None, index_col=(0, 1)).squeeze().to_dict()
-    anx = pd.read_csv('../data/CSV/annexes.csv', header=None, index_col=(0, 1)).squeeze().to_dict()
-    epar = pd.read_csv('../data/CSV/epar.csv', header=None, index_col=(0, 1)).squeeze().to_dict()
-    return dec, anx, epar
+    dec = pd.read_csv('../data/CSV/decision.csv', header=None, index_col=0).squeeze().to_dict()
+    anx = pd.read_csv('../data/CSV/annexes.csv', header=None, index_col=0).squeeze().to_dict()
+    epar = pd.read_csv('../data/CSV/epar.csv', header=None, index_col=0).squeeze().to_dict()
+    med_dict = pd.read_csv('../data/CSV/med_dict.csv', header=None, index_col=0).squeeze().to_dict()
+    return dec, anx, epar, med_dict
 
 
+# TODO: Add a new function in a way that that function gets a medicine and downloads all files for that medicine.
+
+
+# TODO: Fix downloading for epar files
 def run_parallel():
     # Store the result of the csv converting into dictionaries
-    decisions, annexes, epar = read_csv_files()
+    decisions, annexes, epar, med_dict = read_csv_files()
     # Download the decision files, parallel
-    Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "dec", decisions) for medicine in decisions)
-    print("Done with decisions")
+    Parallel(n_jobs=12)(delayed(download_pdfs_ec)(eu_n, "dec", decisions, ast.literal_eval(med_dict[eu_n])) for eu_n in decisions)
+    log.info("Done with decisions")
     # Download the annexes files, parallel
-    Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "anx", annexes) for medicine in annexes)
-    print("Done with Annexes")
-    # Download the epar files, parallel
-    Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "epar", epar) for medicine in epar)
-    print("Done with epars")
+    Parallel(n_jobs=12)(delayed(download_pdfs_ec)(eu_n, "anx", annexes, ast.literal_eval(med_dict[eu_n])) for eu_n in annexes)
+    log.info("Done with Annexes")
+    # # Download the epar files, parallel
+    # Parallel(n_jobs=12)(delayed(download_pdfs)(medicine, "epar", epar) for medicine in epar)
+    # print("Done with epars")
