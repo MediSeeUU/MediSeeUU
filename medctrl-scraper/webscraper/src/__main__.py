8a01cbf9e3a2ee0afe60edc2b20c5704321aecab
TiKee99 <44522314+TiKee99@users.noreply.github.com>
2022-10-18 22:54:25 +0200

    Feature: Made sure that attributes for all medicine types can be scraped

--- medctrl-scraper/webscraper/src/__main__.py
+++ medctrl-scraper/webscraper/src/__main__.py
@@ -15,59 +15,72 @@ import download
 import ec_scraper
 import ema_scraper
 
+# TODO: These variables are for debugging, remove in final
+# Flag variables to indicate whether the webscraper should fill the .csv files or not
+scrape_ec: bool = True
+scrape_ema: bool = False            # Requires scrape_ec to have been run at least once
+download_files: bool = False         # Download pdfs from the obtained links
+parallel_csv_getting: bool = False   # Parallelization is currently broken on Windows. Set to False
+
+# list of the type of medicines that will be scraped
+# NOTE: This was useful for debugging
+scrape_medicine_type: list[ec_scraper.MedicineType] = [ec_scraper.MedicineType.HUMAN_USE_ACTIVE,
+                                                       ec_scraper.MedicineType.HUMAN_USE_WITHDRAWN,
+                                                       ec_scraper.MedicineType.ORPHAN_ACTIVE,
+                                                       ec_scraper.MedicineType.ORPHAN_WITHDRAWN]
+
+# TODO: Logging to monolithic main
 tqdm_format_string = "{l_bar}{bar}| {n_fmt}/{total_fmt} "
 
 # Global configuration of the log file
-logging.basicConfig(filename='webscraper.log', level=logging.INFO)
+log_handler_console = logging.StreamHandler()
+log_handler_file = logging.FileHandler("webscraper.log")
 
+logging.basicConfig(level=logging.INFO, handlers=[log_handler_console, log_handler_file])
+
+# TODO: To __init__? Figure some stuff out
 # Local instance of a logger
 log = logging.getLogger(__name__)
-log_handler = logging.StreamHandler()
-log.addHandler(log_handler)
-
-log.info(f"=== NEW LOG {datetime.today()} ===")
-
-# Create the data dir.
-# The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
-Path("../data/CSV").mkdir(exist_ok=True, parents=True)
-Path("../data/medicines").mkdir(exist_ok=True)
-
-log.info("SUCCESS on Generating directories")
-
-medicine_codes = ec_scraper.scrape_medicines_list(
-    "https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm")
-
-log.info("SUCCESS on scraping all medicine URLs of EC")
 
 
 # Paralleled function for getting the URL codes. They are written to a CSV file
-def get_urls_ec(medicine_url, eu_n):
+def get_urls_ec(medicine_url, eu_n, medicine_type, eu_num_short):
     attempts = 0
     max_attempts = 4
     success = False
-    while attempts < max_attempts and not success:
-        try:
-            # getURLsForPDFAndEMA returns per medicine the urls for the decision and annexes files and for the ema
-            # website.
-            dec_list, anx_list, ema_list, medicine_json, procedures_json = ec_scraper.scrape_medicine_page(medicine_url)
-            with open("../data/CSV/decision.csv", 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([eu_n, dec_list])
-            with open("../data/CSV/annexes.csv", 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([eu_n, anx_list])
-            with open("../data/CSV/ema_urls.csv", 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([eu_n, ema_list])
-            with open("../data/CSV/med_dict.csv", 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([eu_n, med_dict])
-            success = True
-        except Exception:
-            attempts += 1
-            if attempts == max_attempts:
-                log.error(f"failed dec/anx/ema url getting for {eu_n}")
-                break
+
+    if ec_scraper.MedicineType(medicine_type) in scrape_medicine_type:
+        while attempts < max_attempts and not success:
+            try:
+                # getURLsForPDFAndEMA returns per medicine the urls for the decision and annexes files and for the ema
+                # website.
+                dec_list, anx_list, ema_list, attributes_json = \
+                    ec_scraper.scrape_medicine_page(medicine_url, medicine_type)
+
+                with open("../data/CSV/decision.csv", 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([eu_n, dec_list])
+
+                with open("../data/CSV/annexes.csv", 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([eu_n, anx_list])
+
+                with open("../data/CSV/ema_urls.csv", 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([eu_n, ema_list])
+
+                # Creates a directory if the medicine doesn't exist yet, otherwise it just adds the json file to the
+                # existing directory
+                Path(f"../data/medicines/{eu_num_short}").mkdir(exist_ok=True)
+                with open(f"../data/medicines/{eu_num_short}/{eu_num_short}_attributes.json", 'a') as f:
+                    json.dump(attributes_json, f, indent=4)
+
+                success = True
+            except Exception:
+                attempts += 1
+                if attempts == max_attempts:
+                    log.error(f"failed dec/anx/ema url getting for {eu_n}")
+                    break
 
 
 def get_urls_ema(medicine, url: str):
@@ -92,47 +105,58 @@ def get_urls_ema(medicine, url: str):
                 break
 
 
-# TODO: These variables are for debugging, remove in final
-# NOTE: Use the lines of code below to fill all the CSV files.
-# If you have a complete CSV file, this line of code below is not needed.
-scrape_ec: bool = True
+def main():
+    log.info(f"=== NEW LOG {datetime.today()} ===")
 
-# NOTE: Use the lines of code below to fill epar.csv
-# epar.csv will contain the links to the epar pdfs.
-scrape_ema: bool = True
+    # Create the data dir.
+    # The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
+    Path("../data/CSV").mkdir(exist_ok=True, parents=True)
+    Path("../data/medicines").mkdir(exist_ok=True)
 
-# NOTE: Use the line of code below to download all files.
-download_files: bool = False
+    log.info("TASK SUCCESS on Generating directories")
 
-with Parallel(n_jobs=12) as parallel:
-    # TODO: Progressbar needs to be below logging output
-    #       https://stackoverflow.com/questions/6847862/how-to-change-the-format-of-logged-messages-temporarily-in-python
-    # log_handler.setFormatter(logging.Formatter("\r%(message)s"))
-    if scrape_ec:
-        log.info("Scraping all medicines on the EC website")
+    medicine_codes: list[(str, str, int, str)] = ec_scraper.scrape_medicines_list()
 
-        parallel(
-            delayed(get_urls_ec)(medicine_url, eu_n)
-            for (medicine_url, eu_n)
-            in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string)
-        )
-
-        log.info("FINISHED")
+    with Parallel(n_jobs=12) as parallel:
+        # TODO: Progressbar needs to be below logging output
+        #       https://stackoverflow.com/questions/6847862/how-to-change-the-format-of-logged-messages-temporarily-in-python
+        # log_handler.setFormatter(logging.Formatter("\r%(message)s"))
+        if scrape_ec:
+            log.info("ASK START scraping all medicines on the EC website")
+            if parallel_csv_getting:
+                parallel(
+                    delayed(get_urls_ec)(medicine_url, eu_n, medicine_type, eu_num_short)
+                    for (medicine_url, eu_n, medicine_type, eu_num_short)
+                    in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string)
+                )
+            else:
+                for (medicine_url, eu_n, medicine_type, eu_num_short) \
+                        in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string):
+                    get_urls_ec(medicine_url, eu_n, medicine_type, eu_num_short)
+            log.info("TASK FINISHED EC scrape")
 
     if scrape_ema:
         log.info("Scraping all individual medicine pages of EMA")
         ema = pd.read_csv('../data/CSV/ema_urls.csv', header=None, index_col=0).squeeze().to_dict()
+        if parallel_csv_getting:
+            parallel(
+                delayed(get_urls_ema)(url[0], url[1])
+                for url
+                in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
+            )
+        else:
+            for url in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string):
+                get_urls_ema(url[0], url[1])
+        log.info("TASK FINISHED EMA scrape")
 
-        parallel(
-            delayed(get_urls_ema)(url[0], url[1])
-            for url
-            in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
-        )
+        # TODO: Same TODO as above
+        # log_handler.setFormatter(logging.Formatter("%(message)s"))
 
-    # TODO: Same TODO as above
-    # log_handler.setFormatter(logging.Formatter("%(message)s"))
-    log.info("FINISHED")
+    if download_files:
+        download.run_parallel()
 
 
-if download_files:
-    download.run_parallel()
+# Keep the code locally testable by including this.
+# When running this file specifically, the main function will run.
+if __name__ == "__main__":
+    main()
--- medctrl-scraper/webscraper/src/ec_scraper.py
+++ medctrl-scraper/webscraper/src/ec_scraper.py
@@ -2,48 +2,84 @@ import json
 from datetime import date, datetime, timedelta
 from re import DOTALL
 from typing import Type
+from enum import Enum
 
 import regex as re
 import bs4
 import requests
 
 
-def scrape_medicines_list(url: str) -> list[(str, str)]:
-    # Links acquired from "https://ec.europa.eu/health/documents/community-register/html/index_en.htm"
-    # "reg_hum_act.htm" directs to "Union Register of medicinal products for human use" | "Alphabetical"
-    html_active = get_ec_html(url)
-    parsed_json, *_ = get_ec_json_objects(html_active)
+# This is an enum that indicates the medicine type
+# TODO: file that contains all enums so that every script can make use of it
+class MedicineType(Enum):
+    HUMAN_USE_ACTIVE = 0
+    HUMAN_USE_WITHDRAWN = 1
+    ORPHAN_ACTIVE = 2
+    ORPHAN_WITHDRAWN = 3
 
-    url_list: list[(str, str)] = []
 
-    # Each row has an object eu_num: { display, pre, id }
-    # The URL on the EC website uses the pre and id
-    for row in parsed_json:
-        eu_num_short: str = row['eu_num']['pre'] + row['eu_num']['id']
-        eu_num_full: str = (row['eu_num']['display']).replace("/", "-")
+# links to the ec pages that contain medicine codes
+HUMAN_USE_ACTIVE_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm?sort=a"
+HUMAN_USE_WITHDRAWN_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_hum_nact.htm?sort=a"
+HUMAN_USE_REFUSED_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_hum_refus.htm"
+ORPHAN_ACTIVE_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_od_act.htm?sort=a"
+ORPHAN_WITHDRAWN_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_od_nact.htm?sort=a"
+ORPHAN_REFUSED_URL: str = "https://ec.europa.eu/health/documents/community-register/html/reg_od_refus.htm"
 
-        ec_link = f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm"
-        url_list.append((ec_link, eu_num_full))
+
+# Gets the
+def scrape_medicines_list() -> list[(str, str, int, str)]:
+    url_list: list[(str, str, int, str)] = []
+
+    # Loops over all the JSON objects that contain the short eu numbers, so that they can be stored
+    # The first JSON object is for active human use medicine, the second for withdrawn human use,
+    # the third for active orphan and the fourth for withdrawn orphan
+    # This corresponds with the indices of the MedicineType enum
+    medicine_type: int = 0
+    for parsed_json in scrape_active_withdrawn_jsons():
+        # Each row has an object eu_num: { display, pre, id }
+        # The URL on the EC website uses the pre and id
+        for row in parsed_json:
+            eu_num_short: str = row['eu_num']['pre'] + row['eu_num']['id']
+            eu_num_full: str = (row['eu_num']['display']).replace("/", "-")
+
+            ec_link = f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm"
+            url_list.append((ec_link, eu_num_full, medicine_type, eu_num_short))
+            print(eu_num_full)
+        medicine_type += 1
+
+    # If anyone ever wants to scrape the refused medicine, the urls to these medicine can be added with this piece
+    # of code
+    """
+    for parsed_json in scrape_refused_jsons():
+        for row in parsed_json:
+            eu_num_short: str = row['name']['pre'] + str(row['name']['id'])
+            eu_num_full: str = "REFUSED-" + row['name']['display']
+
+            ec_link = f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm"
+            url_list.append((ec_link, eu_num_full))
+            print(eu_num_full)
+    """
 
     return url_list
 
 
-def get_eu_num(url: str) -> str:
-    return url.split('/')[-1].replace(".htm", "")
-    
+# Retrieves a list of JSON objects that are needed to make the urls for all the medicines, except the refused medicines
+def scrape_active_withdrawn_jsons() -> list[json]:
+    active_withdrawn_json_list: list[json] = [get_ec_json_objects(get_ec_html(HUMAN_USE_ACTIVE_URL))[0],
+                                              get_ec_json_objects(get_ec_html(HUMAN_USE_WITHDRAWN_URL))[0],
+                                              get_ec_json_objects(get_ec_html(ORPHAN_ACTIVE_URL))[0],
+                                              get_ec_json_objects(get_ec_html(ORPHAN_WITHDRAWN_URL))[0]]
 
-def scrape_medicine_page(url: str) -> (list[str], list[str], list[str], dict[str, str]):
-    # Retrieves the html from the European Commission product page
-    html_active = get_ec_html(url)
-    medicine_json, procedures_json, *_ = get_ec_json_objects(html_active)
+    return active_withdrawn_json_list
 
-    eu_num = get_eu_num(url)
 
-    # Gets all the necessary information from the medicine_json and procedures_json objects
-    medicine_dict, ema_url_list = get_data_from_medicine_json(medicine_json, eu_num)
-    procedures_dict, dec_url_list, anx_url_list = get_data_from_procedures_json(procedures_json, eu_num)
+# Retrieves a list of JSON objects that are needed to make the urls for all the refused medicines
+def scrape_refused_jsons() -> list[json]:
+    refused_json_list: list[json] = [get_ec_json_objects(get_ec_html(HUMAN_USE_REFUSED_URL))[0],
+                                     get_ec_json_objects(get_ec_html(ORPHAN_REFUSED_URL))[0]]
 
-    return dec_url_list, anx_url_list, ema_url_list, medicine_dict, procedures_dict
+    return refused_json_list
 
 
 # Fetches the html from the EC website for a certain medicine
@@ -81,30 +117,59 @@ def get_ec_json_objects(html_active: requests.Response) -> list[json]:
 
     parsed_jsons: list[json] = list(map(lambda plaintext: json.loads(plaintext), plaintext_jsons))
     return parsed_jsons
+    
+
+# Gets all the urls and data for a single medicine
+def scrape_medicine_page(url: str, medicine_type: MedicineType) -> (list[str], list[str], list[str], json):
+    # Retrieves the html from the European Commission product page
+    html_active = get_ec_html(url)
+    medicine_json, procedures_json, *_ = get_ec_json_objects(html_active)
+
+    eu_num = get_eu_num(url)
+
+    # Gets all the necessary information from the medicine_json and procedures_json objects
+    medicine_dict, ema_url_list = get_data_from_medicine_json(medicine_json, eu_num, medicine_type)
+    procedures_dict, dec_url_list, anx_url_list = get_data_from_procedures_json(procedures_json, eu_num)
+
+    # combine the attributes from both dictionaries files in a single JSON file
+    all_attributes_dict = medicine_dict | procedures_dict
+    all_attributes_dump = json.dumps(all_attributes_dict, indent=4)
+    all_attributes_json = json.loads(all_attributes_dump)
+
+    print(eu_num + ": FINISHED")
+
+    return dec_url_list, anx_url_list, ema_url_list, all_attributes_json
+
+
+# Helper function that gets the short EU number from a url
+# TODO: pass the short EU number in the function, instead of retrieving it this way
+def get_eu_num(url: str) -> str:
+    return url.split('/')[-1].replace(".htm", "")
 
 
 # The medicine_json object from the EC contains some important information that needs to be scraped
 # It loops through the JSON object and finds all the attributes, so that they can be used and stored
-def get_data_from_medicine_json(medicine_json: json, eu_num: str) -> (json, list[str]):
+def get_data_from_medicine_json(medicine_json: json, eu_num: str, medicine_type: MedicineType) \
+        -> (dict[str, str], list[str]):
     medicine_dict: dict[str, str] = {}
     ema_url_list: list[str] = []
 
     # Orphan medicine don't always have ATC codes, therefore it is set to a standard value
-    medicine_dict["atc_code"] = None
+    medicine_dict["atc_code"] = ""
 
     for row in medicine_json:
         match row["type"]:
             case "name":
-                medicine_dict["eu_aut_status"] = row["meta"]["status_name"]
-                medicine_dict["eu_brand_name_current"] = row["value"]
-                medicine_dict["orphan_status"] = row["meta"]["pre"]
-                medicine_dict["status_type"] = row["meta"]["status_type"].replace("g", "a").replace("r", "w")
+                eu_aut_status: str = row["meta"]["status_name"]
+                eu_brand_name_current: str = row["value"]
+                status_type: str = row["meta"]["status_type"].replace("g", "a").replace("r", "w")
+
             case "eu_num":
-                medicine_dict["eu_pnumber"] = row["value"]
+                eu_pnumber: str = row["value"]
 
             case "inn":
-                # Sometimes the active substance is written with italics, therefore it is removed with a regex
-                medicine_dict["active_substance"] = re.sub(re.compile('<.*?>'), '', row["value"])
+                # Sometimes the active substance is written with italics, therefore it is removed with a RegEx
+                active_substance: str = re.sub(re.compile('<.*?>'), '', row["value"])
 
             case "indication":
                 # If at any point in the future, information about the indication needs to be stored,
@@ -112,25 +177,59 @@ def get_data_from_medicine_json(medicine_json: json, eu_num: str) -> (json, list
                 pass
 
             case "mah":
-                medicine_dict["eu_mah_current"] = row["value"]
+                eu_mah_current: str = row["value"]
 
             case "atc":
-                meta_obj = row["meta"][0]
-                medicine_dict["atc_code"] = meta_obj[-1]["code"]
+                atc_code: str = row["meta"][0][-1]["code"]
 
             case "ema_links":
                 for json_obj in row["meta"]:
                     ema_url_list.append(json_obj["url"])
 
-    return json.dumps(medicine_dict, indent = 4), ema_url_list
+    # TODO: logging when an attribute is not found
+    # Currently when an attribute is not found it is simply printed to the console
+    try:
+        medicine_dict["eu_aut_status"] = eu_aut_status
+    except:
+        print(eu_num + ": couldn't  find authorization status")
+    try:
+        medicine_dict["eu_brand_name_current"] = eu_brand_name_current
+    except:
+        print(eu_num + ": couldn't find current brand name")
+    try:
+        medicine_dict["status_type"] = status_type
+    except:
+        print(eu_num + ": couldn't find status type")
+    try:
+        medicine_dict["eu_pnumber"] = eu_pnumber
+    except:
+        print(eu_num + ": couldn't find EU product number")
+    try:
+        medicine_dict["active_substance"] = active_substance
+    except:
+        print(eu_num + ": couldn't find active substance")
+    try:
+        medicine_dict["eu_mah_current"] = eu_mah_current
+    except:
+        print(eu_num + ": couldn't find current marketing authorization holder")
+    try:
+        # Orphan medicine never have ATC codes, therefore it will insert a dummy value for them
+        if medicine_type == MedicineType.HUMAN_USE_ACTIVE or medicine_type == MedicineType.HUMAN_USE_WITHDRAWN:
+            medicine_dict["atc_code"] = atc_code
+        else:
+            medicine_dict["atc_code"] = "not applicable"
+    except:
+        print(eu_num + ": couldn't find ATC code")
+
+    return medicine_dict, ema_url_list
 
 
 # TODO: Change this function so that it not only gets the dec and anx urls, but other data as well
-def get_data_from_procedures_json(procedures_json: json, eu_num: str) -> (json, list[str], list[str]):
+def get_data_from_procedures_json(procedures_json: json, eu_num: str) -> (dict[str, str], list[str], list[str]):
     # The necessary urls and other data will be saved in these lists and dictionary
     dec_url_list: list[str] = []
     anx_url_list: list[str] = []
-    procedures_dict: list[str, str] = {}
+    procedures_dict: dict[str, str] = {}
     # The list of ema numbers will be saved, so that the right EMA number can be chosen
     ema_numbers: list[str] = []
     # This information is needed to determine the initial authorization type
@@ -191,7 +290,8 @@ def get_data_from_procedures_json(procedures_json: json, eu_num: str) -> (json, 
     # From the list of EMA numbers, the right one is chosen and its certainty determined
     ema_number, ema_number_certainty = determine_ema_number(ema_numbers)
 
-    # All the attribute values are put in the dictionary
+    # TODO: logging when an attribute is not found
+    # Currently when an attribute is not found it is simply printed to the console
     try:
         procedures_dict["eu_aut_date"] = eu_aut_date
     except:
@@ -213,7 +313,7 @@ def get_data_from_procedures_json(procedures_json: json, eu_num: str) -> (json, 
     except:
         print(eu_num + ": couldn't find ema number certainty")
 
-    return json.dumps(procedures_dict, indent = 4), dec_url_list, anx_url_list
+    return procedures_dict, dec_url_list, anx_url_list
 
 
 # Determines the current authorization type
@@ -244,11 +344,10 @@ def determine_aut_type(year: int, is_exceptional: bool, is_conditional: bool) ->
 # EMA numbers are formatted, so that only the relevant part of the EMA number remains
 # Sometimes, there are two ema numbers in one column, therefore we return a list of strings 
 def format_ema_number(ema_number: str) -> list[str]:
-    ema_numbers: list[str]= ema_number.split(", ")
+    ema_numbers: list[str] = ema_number.split(", ")
     ema_numbers_formatted: list[str] = []
 
     for en in ema_numbers:
-        
         try:
             # REGEX that gets only the relevant part of the EMA numbers
             ema_number_formatted = re.findall(r"EME?A\/(?:H\/(?:C|\w*-\w*)\/\w*|OD\/\w*(?:\/\w*)?)", en, re.DOTALL)[0]
@@ -277,10 +376,12 @@ def determine_ema_number(ema_numbers: list[str]) -> (str, float):
     return most_occurring_item, fraction
 
 
+# uncomment this when you want to test if data is retrieved for a certain medicine
+"""
 def test_code(eu_num_short: str):
     ec_link = f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm"
     scrape_medicine_page(ec_link)
 
 
-# uncomment this when you want to test if data is retrieved for a certain medicine
 test_code("h544")
+"""
