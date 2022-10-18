210d0ad2a059cf3266891772b63e627d15337575
Matthijs Boers <matthijsje.boers@gmail.com>
2022-10-18 16:45:44 +0200

    Setup for monolithic __main__.py

--- medctrl-scraper/webscraper/src/__main__.py
+++ medctrl-scraper/webscraper/src/__main__.py
@@ -1,6 +1,3 @@
-# Entry point of the Python code
-# This will run if you run `python3 .` in the directory
-
 import csv
 import json
 import logging
@@ -15,6 +12,14 @@ import download
 import ec_scraper
 import ema_scraper
 
+# TODO: These variables are for debugging, remove in final
+# Flag variables to indicate whether the webscraper should fill the .csv files or not
+scrape_ec: bool = False
+scrape_ema: bool = False            # Requires scrape_ec to have been run at least once
+download_files: bool = True         # Download pdfs from the obtained links
+parallel_csv_getting: bool = True   # Parallelization is currently broken on Windows. Set to False
+
+# TODO: Logging to monolithic main
 tqdm_format_string = "{l_bar}{bar}| {n_fmt}/{total_fmt} "
 
 # Global configuration of the log file
@@ -23,22 +28,9 @@ log_handler_file = logging.FileHandler("webscraper.log")
 
 logging.basicConfig(level=logging.INFO, handlers=[log_handler_console, log_handler_file])
 
+# TODO: To __init__? Figure some stuff out
 # Local instance of a logger
 log = logging.getLogger(__name__)
-
-log.info(f"=== NEW LOG {datetime.today()} ===")
-
-# Create the data dir.
-# The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
-Path("../data/CSV").mkdir(exist_ok=True, parents=True)
-Path("../data/medicines").mkdir(exist_ok=True)
-
-log.info("TASK SUCCESS on Generating directories")
-
-medicine_codes = ec_scraper.scrape_medicines_list(
-    "https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm")
-
-log.info("TASK SUCCESS on scraping all medicine URLs of EC")
 
 
 # Paralleled function for getting the URL codes. They are written to a CSV file
@@ -93,55 +85,60 @@ def get_urls_ema(medicine, url: str):
                 break
 
 
-# TODO: These variables are for debugging, remove in final
-# NOTE: Use the lines of code below to fill all the CSV files.
-# If you have a complete CSV file, this line of code below is not needed.
-scrape_ec: bool = False
+def main():
+    log.info(f"=== NEW LOG {datetime.today()} ===")
 
-# NOTE: Use the lines of code below to fill epar.csv
-# epar.csv will contain the links to the epar pdfs.
-scrape_ema: bool = False
+    # Create the data dir.
+    # The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
+    Path("../data/CSV").mkdir(exist_ok=True, parents=True)
+    Path("../data/medicines").mkdir(exist_ok=True)
 
-# NOTE: Use the line of code below to download all files.
-download_files: bool = True
+    log.info("TASK SUCCESS on Generating directories")
 
-# NOTE: Use the line of code below to fill CSV files parallel:
-# for Windows users: set to 'False'
-parallel_csv_getting: bool = True
+    medicine_codes = ec_scraper.scrape_medicines_list(
+        "https://ec.europa.eu/health/documents/community-register/html/reg_hum_act.htm")
 
-with Parallel(n_jobs=12) as parallel:
-    # TODO: Progressbar needs to be below logging output
-    #       https://stackoverflow.com/questions/6847862/how-to-change-the-format-of-logged-messages-temporarily-in-python
-    # log_handler_console.setFormatter(logging.Formatter("\r%(message)s"))
-    if scrape_ec:
-        log.info("TASK START scraping all medicines on the EC website")
-        if parallel_csv_getting:
-            parallel(
-                delayed(get_urls_ec)(medicine_url, eu_n)
-                for (medicine_url, eu_n)
-                in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string)
-            )
-        else:
-            for (medicine_url, eu_n) in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string):
-                get_urls_ec(medicine_url, eu_n)
-        log.info("TASK FINISHED EC scrape")
+    log.info("TASK SUCCESS on scraping all medicine URLs of EC")
 
-    if scrape_ema:
-        log.info("TASK START scraping all individual medicine pages of EMA")
-        ema = pd.read_csv('../data/CSV/ema_urls.csv', header=None, index_col=0).squeeze().to_dict()
-        if parallel_csv_getting:
-            parallel(
-                delayed(get_urls_ema)(url[0], url[1])
-                for url
-                in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
-            )
-        else:
-            for url in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string):
-                get_urls_ema(url[0], url[1])
-        log.info("TASK FINISHED EMA scrape")
+    with Parallel(n_jobs=12) as parallel:
+        # TODO: Progressbar needs to be below logging output
+        #       https://stackoverflow.com/questions/6847862/how-to-change-the-format-of-logged-messages-temporarily-in-python
+        # log_handler_console.setFormatter(logging.Formatter("\r%(message)s"))
+        if scrape_ec:
+            log.info("TASK START scraping all medicines on the EC website")
+            if parallel_csv_getting:
+                parallel(
+                    delayed(get_urls_ec)(medicine_url, eu_n)
+                    for (medicine_url, eu_n)
+                    in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string)
+                )
+            else:
+                for (medicine_url, eu_n) in tqdm.tqdm(medicine_codes, bar_format=tqdm_format_string):
+                    get_urls_ec(medicine_url, eu_n)
+            log.info("TASK FINISHED EC scrape")
 
-    # TODO: Same TODO as above
-    # log_handler_console.setFormatter(logging.Formatter("%(message)s"))
+        if scrape_ema:
+            log.info("TASK START scraping all individual medicine pages of EMA")
+            ema = pd.read_csv('../data/CSV/ema_urls.csv', header=None, index_col=0).squeeze().to_dict()
+            if parallel_csv_getting:
+                parallel(
+                    delayed(get_urls_ema)(url[0], url[1])
+                    for url
+                    in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
+                )
+            else:
+                for url in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string):
+                    get_urls_ema(url[0], url[1])
+            log.info("TASK FINISHED EMA scrape")
 
-if download_files:
-    download.download_all(parallel_download=True)
+        # TODO: Same TODO as above
+        # log_handler_console.setFormatter(logging.Formatter("%(message)s"))
+
+    if download_files:
+        download.download_all(parallel_download=True)
+
+
+# Keep the code locally testable by including this.
+# When running this file specifically, the main function will run.
+if __name__ == "__main__":
+    main()
