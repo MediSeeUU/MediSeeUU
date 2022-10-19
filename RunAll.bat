96037d75fcde0702a267b1e93d23f26a9d29af2f
miloravi <57906122+miloravi@users.noreply.github.com>
2022-10-19 15:46:08 +0200

    Fixed: readded some deleted bat files

---
+++ RunAll.bat
@@ -0,0 +1,5 @@
+@REM This program has been developed by students from the bachelor Computer Science at
+@REM Utrecht University within the Software Project course.
+@REM © Copyright Utrecht University (Department of Information and Computing Sciences)
+start run-frontend.bat
+start run-backend.bat
