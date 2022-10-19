393559f6d0b0027238eeedc21972c1f323353116
elioverhoef <49610595+elioverhoef@users.noreply.github.com>
2022-10-19 15:26:25 +0200

    Update xml_converter om special characters te filteren

--- scraping/pdf_module/pdf_scraper/pdf_xml_converter.py
+++ scraping/pdf_module/pdf_scraper/pdf_xml_converter.py
@@ -82,48 +82,28 @@ def remove_illegal_characters(string: str) -> str:
         if encoded_char == "":
             continue
 
-        if encoded_char >= 0x1 and encoded_char <= 0x8:
+        if bytes.fromhex("01") <= encoded_char <= bytes.fromhex("08"):
             is_illegal = True
 
-        if encoded_char >= 0xB and encoded_char <= 0xC:
+        if bytes.fromhex("0B") <= encoded_char <= bytes.fromhex("0C"):
             is_illegal = True
 
-        if encoded_char >= 0xE and encoded_char <= 0x1F:
+        if bytes.fromhex("0E") <= encoded_char <= bytes.fromhex("1F"):
             is_illegal = True
 
-        if encoded_char >= 0x7F and encoded_char <= 0x84:
+        if bytes.fromhex("7F") <= encoded_char <= bytes.fromhex("84"):
             is_illegal = True
 
-        if encoded_char >= 0x86 and encoded_char <= 0x9F:
+        if bytes.fromhex("86") <= encoded_char <= bytes.fromhex("9F"):
             is_illegal = True
 
-        if encoded_char in [0x00, 0xEFFF, 0xEFFF]:
+        if encoded_char in ["00", "EF FF", "EF FF"]:
             is_illegal = True
 
         if not is_illegal:
             non_illegal_string += character
     
     return non_illegal_string
-
-
-# def remove_illegal_characters(string: str) -> str:
-#     illegal_unicodes = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
-#                         (0x7F, 0x84), (0x86, 0x9F),
-#                         (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
-#     if sys.maxunicode >= 0x10000:
-#         illegal_unicodes.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
-#                                  (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
-#                                  (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
-#                                  (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
-#                                  (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
-#                                  (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
-#                                  (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
-#                                  (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)])
-
-#     illegal_ranges = [fr'{chr(low)}-{chr(high)}' for (low, high) in illegal_unicodes]
-#     xml_illegal_character_regex = '[' + ''.join(illegal_ranges) + ']'
-#     illegal_xml_chars_re = re.compile(xml_illegal_character_regex)
-#     return illegal_xml_chars_re.sub('', string)
 
 
 def print_xml_tag_open(xml_tag: str, file: TextIO, attributes: str = ""):
