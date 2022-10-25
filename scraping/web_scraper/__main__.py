b1cca4d961f9a860586ff130e9105368c183d8a6
Matthijs Boers <matthijsje.boers@gmail.com>
2022-10-25 15:49:13 +0200

    Generalise Jsonhelper and adapt into __main__.py

--- scraping/web_scraper/__main__.py
+++ scraping/web_scraper/__main__.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import logging
 from pathlib import Path
@@ -12,7 +11,7 @@ import download
 import ec_scraper
 import ema_scraper
 import utils
-from json_helper import JsonHelper
+import json_helper
 
 # TODO: These variables are for debugging, remove in final
 # Flag variables to indicate whether the webscraper should fill the .csv files or not
@@ -40,46 +39,54 @@ logging.basicConfig(level=logging.INFO, handlers=[log_handler_console, log_handl
 # TODO: To __init__? Figure some stuff out
 log = logging.getLogger(__name__)
 
-url_file = JsonHelper(path="CSV/urls.json")
+url_file = json_helper.JsonHelper(path="CSV/urls.json")
 
 
 # Paralleled function for getting the URL codes. They are written to a CSV file
 # TODO: unmarked type for medicine_type
 def get_urls_ec(medicine_url: str, eu_n: str, medicine_type, data_path):
-    if ec_scraper.MedicineType(medicine_type) in scrape_medicine_type:
-        # getURLsForPDFAndEMA returns per medicine the urls
-        # for the decision and annexes files and for the ema website.
-        dec_list, anx_list, ema_list, attributes_dict = \
-            ec_scraper.scrape_medicine_page(medicine_url, ec_scraper.MedicineType(medicine_type))
+    # A list of the medicine types we want to scrape is defined in this file
+    # If the entered medicine_type is not in that list, this method will ignore
+    if ec_scraper.MedicineType(medicine_type) not in scrape_medicine_type:
+        return
 
-        # TODO: Common name structure?
-        url_json: dict[str, list[str]] = {
-            "dec": dec_list,
-            "anx": anx_list,
-            "ema": ema_list
+    # dec_ anx_ and ema_list are lists of URLs to PDF files
+    # Attributes_dict is a dictionary containing the attributes scraped from the EC page
+    dec_list, anx_list, ema_list, attributes_dict = \
+        ec_scraper.scrape_medicine_page(medicine_url, ec_scraper.MedicineType(medicine_type))
+
+    # TODO: Common name structure?
+    url_json: dict[str, list[str]] = {
+        eu_n: {
+            "aut_url": dec_list,
+            "smpc_url": anx_list,
+            "ema_url": ema_list
         }
+    }
 
-        url_file.save_single_url(eu_n, url_json)
+    url_file.add_to_dict(url_json)
 
-        # Creates a directory if the medicine doesn't exist yet,
-        # otherwise it just adds the json file to the existing directory
-        Path(f"{data_path}/{eu_n}").mkdir(exist_ok=True)
-        with open(f"{data_path}/{eu_n}/{eu_n}_attributes.json", 'w') as f:
-            json.dump(attributes_dict, f, indent=4)
+    # Creates a directory if the medicine doesn't exist yet,
+    # otherwise it just adds the json file to the existing directory
+    Path(f"{data_path}/{eu_n}").mkdir(exist_ok=True)
+    with open(f"{data_path}/{eu_n}/{eu_n}_attributes.json", 'w') as f:
+        json.dump(attributes_dict, f, indent=4)
 
 
 def get_urls_ema(eu_n: str, url: str):
-    pdf_url: dict = {"epar": ema_scraper.pdf_links_from_url(url)}
-    url_file.save_single_url(eu_n, pdf_url)
+    pdf_url: dict = {
+        eu_n: {
+            "epar_url": ema_scraper.pdf_links_from_url(url)
+        }
+    }
+    url_file.add_to_dict(pdf_url)
 
 
-def main(data_filepath='../../data'):
+def main(data_filepath: str = '../../data'):
     log.info(f"=== NEW LOG {datetime.today()} ===")
 
-    # TODO: Remove mkdir after it is moved to monolithic main
-    # Create the data dir.
-    # The ' exist_ok' option ensures no errors thrown if this is not the first time the code runs.
     Path("CSV").mkdir(exist_ok=True, parents=True)
+    # TODO: Remove mkdir filepath after it is moved to monolithic main
     Path(data_filepath).mkdir(exist_ok=True, parents=True)
 
     log.info("TASK SUCCESS on Generating directories")
@@ -106,19 +113,23 @@ def main(data_filepath='../../data'):
                     get_urls_ec(medicine_url, eu_n, medicine_type)
             log.info("TASK FINISHED EC scrape")
 
-    if scrape_ema:
-        log.info("Scraping all individual medicine pages of EMA")
-        ema = pd.read_csv('CSV/ema_urls.csv', header=None, index_col=0, on_bad_lines='skip').squeeze().to_dict()
-        if use_parallelization:
-            parallel(
-                delayed(get_urls_ema)(url[0], url[1])
-                for url
-                in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string)
-            )
-        else:
-            for url in tqdm.tqdm(ema.items(), bar_format=tqdm_format_string):
-                utils.exception_retry(get_urls_ema(url[0], url[1]))
-        log.info("TASK FINISHED EMA scrape")
+        if scrape_ema:
+            log.info("Scraping all individual medicine pages of EMA")
+            # Transform JSON object into list of (eu_n, url)
+            ema_urls = [(eu_n, url)
+                        for eu_n, value_dict in url_file.local_dict.items()
+                        for url in value_dict["ema_url"]]
+
+            if use_parallelization:
+                parallel(
+                    delayed(get_urls_ema)(eu_n, urls["ema_url"])
+                    for eu_n, urls
+                    in tqdm.tqdm(ema_urls, bar_format=tqdm_format_string)
+                )
+            else:
+                for eu_n, url in tqdm.tqdm(ema_urls, bar_format=tqdm_format_string):
+                    utils.exception_retry(get_urls_ema(eu_n, url))
+            log.info("TASK FINISHED EMA scrape")
 
         # TODO: Same TODO as above
         # log_handler.setFormatter(logging.Formatter("%(message)s"))
--- scraping/web_scraper/json_helper.py
+++ scraping/web_scraper/json_helper.py
@@ -1,31 +1,81 @@
 import json
-
-# TODO: Rewrite documentation when QA comes up with a standard
+import os.path
 
 
 class JsonHelper:
     def __init__(self, path: str):
         self.path = path
+        self.local_dict: dict = self.load_json()
 
-    def save_urls(self, url_dict_list: dict[str, [dict]]):
+    def save_dict(self):
+        """
+        Save the dictionary to disk.
+        Meant to be run when no more changes are expected to be made.
+
+        Returns: None
+        """
+        # 'w+' mode creates or overwrites a file with the name
         with open(self.path, 'w+') as file:
-            json.dump(url_dict_list, file)
+            json.dump(self.local_dict, file)
 
-    def save_single_url(self, eu_num: str, url_dict: dict, overwrite_existing: bool = False):
-        existing_json = self.load_urls()
+    def load_json(self) -> dict[str, [dict]]:
+        """
+        Loads the json from disk with the path given on class initialisation
+        If no json file exists at the path, return an empty dictionary.
 
-        if not overwrite_existing:
-            # |= operator appends dict to another dict
-            existing_json[eu_num] |= url_dict
-        else:
-            existing_json[eu_num] = url_dict
+        Returns: Dictionary representing the json at the path
+        """
+        if not os.path.isfile(self.path):
+            return {}
 
-        self.save_urls(existing_json)
-
-    def load_urls(self) -> dict[str, [dict]]:
         with open(self.path, 'r') as file:
             return json.load(file)
 
-    def load_single_url(self, eu_num: str) -> dict:
-        full_dict = self.load_urls()
-        return full_dict[eu_num]
+    def add_to_dict(self, new_dict: dict):
+        """
+        Wrapper for merge_nested_dict. Used the self.local_dict as old_dict
+
+        Args:
+            new_dict: Dictionary used as second dictionary in merge_nested_dict
+
+        Returns: None
+        """
+        self.merge_nested_dict(self.local_dict, new_dict)
+
+    def overwrite_dict(self, new_dict: dict):
+        """
+        Overwrites the dictionary stored in the class with the input
+
+        Args:
+            new_dict: Dictionary to replace the existing dictionary
+
+        Returns: None
+        """
+        self.local_dict = new_dict
+
+    def merge_nested_dict(self, old_dict: dict, new_dict: dict):
+        """
+        Recursive function for merging two dictionaries. Merges the attributes of all nested dictionaries. Also merges
+        lists in the dictionaries.
+
+        Args:
+            old_dict: First dictionary. Non-list and -dictionary elements get overwritten by elements from new_dict.
+            new_dict: Second dictionary. Non-list and -dictionary elements from this dictionary will overwrite items
+                         from the old_dict
+
+        Returns: Merged dictionary from the two inputs
+        """
+        for key in new_dict.keys():
+            if key in old_dict and isinstance(old_dict[key], dict) and isinstance(new_dict[key], dict):
+                # Recursive call if both items are dictionaries
+                old_dict = self.merge_nested_dict(old_dict[key], new_dict[key])
+
+            elif key in old_dict and isinstance(old_dict[key], list) and isinstance(new_dict[key], list):
+                # Edge case call if both items are lists
+                old_dict[key] += new_dict[key]
+
+            else:
+                # Normal data replacement call
+                old_dict[key] = new_dict[key]
+
+        return old_dict
