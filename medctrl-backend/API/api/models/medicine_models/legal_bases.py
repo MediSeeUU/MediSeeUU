a8e3edbe34f5aae8da078dd2ce6af55a3df8d77e
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-10-27 16:20:32 +0200

    added unique constraint to legal bases model

--- medctrl-backend/API/api/models/medicine_models/legal_bases.py
+++ medctrl-backend/API/api/models/medicine_models/legal_bases.py
@@ -26,3 +26,6 @@ class LegalBases(models.Model):
 
     class Meta:
         db_table = "legal_bases"
+        constraints = [
+            models.UniqueConstraint(fields=['eu_pnumber', 'eu_legal_basis'], name="unique together")
+        ]
