ad1efb1f57f065fc6f82410ef7003861449b9641
Yfke Smit <smityfke@gmail.com>
2022-10-27 15:06:20 +0200

    downloading in webscraper now uses the json files instead of csv

--- scraping/web_scraper/download.py
+++ scraping/web_scraper/download.py
@@ -8,6 +8,7 @@ import requests
 from joblib import Parallel, delayed
 
 import regex as re
+import scraping.web_scraper.json_helper as json_helper
 
 log = logging.getLogger("webscraper.ec_scraper")
 
@@ -29,53 +30,34 @@ def download_pdf_from_url(url: str, eu_num: str, filename_elements: list[str]):
 
 
 # Download pdfs using the dictionaries created from the CSV files
-def download_pdfs_ec(eu_num: str, pdf_type: str, pdf_url_dict: dict[str, str], med_dict: dict[str, str]):
+def download_pdfs_ec(eu_num: str, pdf_type: str, pdf_urls: list[str], med_dict: dict[str, str]):
     file_counter = 0
-    for url in ast.literal_eval(pdf_url_dict[eu_num]):
+    for url in pdf_urls:
         filename_elements = [med_dict["orphan_status"], med_dict["status_type"], pdf_type, str(file_counter)]
         download_pdf_from_url(url, eu_num, filename_elements)
         file_counter += 1
 
 
-def download_pdfs_ema(eu_num: str, epar_dict: dict[str, str], med_dict: dict[str, str]):
-    if eu_num in epar_dict:
-        url = epar_dict[eu_num]
-        pdf_type = re.findall(r"(?<=epar-)(.*)(?=_en)", url)[0]
+def download_pdfs_ema(eu_num: str, epar_url: str, med_dict: dict[str, str]):
+    if epar_url != '':
+        pdf_type = re.findall(r"(?<=epar-)(.*)(?=_en)", epar_url)[0]
         filename_elements = [med_dict["orphan_status"], med_dict["status_type"], pdf_type]
-        download_pdf_from_url(url, eu_num, filename_elements)
+        download_pdf_from_url(epar_url, eu_num, filename_elements)
     else:
         log.info(f"no ema documents available for {eu_num}")
 
 
-# Function for reading the CSV contents back into dictionaries that can be used for downloading.
-def read_csv_files():
-    dec = pd.read_csv('CSV/decision.csv', header=None, index_col=0, lineterminator='\n', on_bad_lines='skip').squeeze().to_dict()
-    anx = pd.read_csv('CSV/annexes.csv', header=None, index_col=0, lineterminator='\n', on_bad_lines='skip').squeeze().to_dict()
-    epar = pd.read_csv('CSV/epar.csv', header=None, index_col=0, lineterminator='\n', on_bad_lines='skip').squeeze().to_dict()
-    med_dict = pd.read_csv('CSV/med_dict.csv', header=None, index_col=0, on_bad_lines='skip').squeeze().to_dict()
-    return dec, anx, epar, med_dict
-
-
-# helper function that converts a windows csv file to linux.
-def dos2unix(f_in):
-    with open('CSV/temp.csv', "w") as f_out:
-        with open(f_in, "r") as fin:
-            for line in fin:
-                line = line.replace('\r\n', '\n')
-                f_out.write(line)
-    os.rename('CSV/temp.csv', f_in)
-
-# TODO: Add a new function in a way that that function gets a medicine and downloads all files for that medicine.
-def download_medicine_files(eu_n: str, dec: dict[str, list[str]], anx: dict[str, list[str]], epar: dict[str, str], med_info: dict[str, str]):
+def download_medicine_files(eu_n: str, url_dict: dict[str, [str]]):
     log.debug(eu_n)
     attempts = 0
     max_attempts = 4
     success = False
+    attr_dict = (json_helper.JsonHelper(path=f"../../data/{eu_n}/{eu_n}_attributes.json")).load_json()
     while attempts < max_attempts and not success:
         try:
-            download_pdfs_ec(eu_n, "dec", dec, med_info)
-            download_pdfs_ec(eu_n, "anx", anx, med_info)
-            download_pdfs_ema(eu_n, epar, med_info)
+            download_pdfs_ec(eu_n, "dec", url_dict["aut_url"], attr_dict)
+            download_pdfs_ec(eu_n, "anx", url_dict["smpc_url"], attr_dict)
+            download_pdfs_ema(eu_n, url_dict["epar_url"], attr_dict)
             success = True
             log.info(f"Downloaded all files for {eu_n}")
         except Exception:
@@ -84,20 +66,25 @@ def download_medicine_files(eu_n: str, dec: dict[str, list[str]], anx: dict[str,
 
 
 def download_all(parallel_download: bool):
-    dos2unix('CSV/epar.csv')
+    # dos2unix('CSV/epar.csv')
     # Store the result of the csv converting into dictionaries
-    decisions, annexes, epar, med_dict = read_csv_files()
+    # decisions, annexes, epar, med_dict = read_csv_files()
     log.info("TASK START downloading pdf files from fetched urls from EC and EMA")
+    urls_dict = (json_helper.JsonHelper(path="../../scraping/web_scraper/CSV/urls.json")).load_json()
     if parallel_download:
         with Parallel(n_jobs=12) as parallel:
             # Download the decision files, parallel
             parallel(
-                delayed(download_medicine_files)(eu_n, decisions, annexes, epar, ast.literal_eval(med_dict[eu_n]))
+                delayed(download_medicine_files)(eu_n, urls_dict[eu_n])
                 for eu_n
-                in med_dict
+                in urls_dict
             )
 
     else:
-        for eu_n in med_dict:
-            download_medicine_files(eu_n, decisions, annexes, epar, ast.literal_eval(med_dict[eu_n]))
+        for eu_n in urls_dict:
+            download_medicine_files(eu_n, urls_dict[eu_n])
     log.info("TASK FINISHED downloading pdf files")
+
+
+
+
