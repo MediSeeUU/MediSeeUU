6a30078b84e1d9c522844407c2193afef0692272
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-10-27 11:51:35 +0200

    added documentation for pdf scraper

---
+++ scraping/pdf_module/docs/sphinx-source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../../../'))
+sys.path.insert(0, os.path.abspath('../../../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PDF Scraper'
+copyright = '2022, Medisee'
+author = 'Medisee'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+templates_path = ['_templates']
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    ]
+
+autosummary_generate = True
+
+exclude_patterns = [
+    '**/venv',
+    '**/__pychache__',
+    '**/node_modules',
+    ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
