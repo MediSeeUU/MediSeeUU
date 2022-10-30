5ca54bb36a0570abe6f72123f613f616da22ad38
TiKee99 <44522314+TiKee99@users.noreply.github.com>
2022-10-30 11:09:28 +0100

    Testing: Web scraper - Fixed unit test that were not running because of conflicting libraries

--- scraping/web_scraper/ec_scraper.py
+++ scraping/web_scraper/ec_scraper.py
@@ -321,7 +321,7 @@ def get_data_from_procedures_json(procedures_json: json, eu_num: str) -> (dict[s
 
     # Gets the oldest authorization procedure (which is the first in the list) and gets the date from there
     eu_aut_datetime: datetime = datetime.strptime(procedures_json[0]["decision"]["date"], '%Y-%m-%d')
-    eu_aut_date: str = datetime.strftime(eu_aut_datetime, '%m-%d-%Y')
+    eu_aut_date: str = datetime.strftime(eu_aut_datetime, '%d-%m-%Y')
 
     # From the list of EMA numbers, the right one is chosen and its certainty determined
     ema_number, ema_number_certainty = determine_ema_number(ema_numbers)
--- tests/webscraper_tests/test_ec_scraper.py
+++ tests/webscraper_tests/test_ec_scraper.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, mock
 import sys
+import json
 import pytest
 import regex as re
 from scraping.web_scraper import ec_scraper as ec
@@ -19,14 +20,20 @@ class TestEcScraper(TestCase):
         check = re.findall(r"[ho]\d+", last_url)[0]
         self.assertEqual(check, last_url, msg="last url not in correct format")
 
+
     def test_scrape_active_withdrawn_jsons(self):
-        self.fail()
+        json_list: list[json] = ec.scrape_active_withdrawn_jsons()
+        self.assertIsNotNone(json_list, msg="json list is empty")
+
 
     def test_scrape_refused_jsons(self):
-        self.fail()
+        json_list: list[json] = ec.scrape_refused_jsons()
+        self.assertIsNotNone(json_list, msg="json list is empty")
+
 
     def test_get_ec_json_objects(self):
         self.fail()
+
 
     def test_scrape_medicine_page(self):
         url = 'https://ec.europa.eu/health/documents/community-register/html/h273.htm'
@@ -65,108 +72,128 @@ class TestEcScraper(TestCase):
         self.assertEqual(decision, dec_result, "incorrect decision result")
         self.assertEqual(annex, anx_result, "incorrect annex result")
 
-    # Unit test that checks whether the correct data is retrieved for a certain medicine.
-    @pytest.mark.parametrize(
-        "eu_num_short, "
-        "exp_atc_code, "
-        "exp_active_substance, "
-        "exp_eu_pnumber, "
-        "exp_eu_aut_status, "
-        "exp_eu_brand_name_current, "
-        "exp_eu_mah_current",
-        [
-            ("h412",
-             "B03XA01",
-             "epoetin alfa",
-             "EU/1/07/412",
-             "ACTIVE",
-             "Abseamed",
-             "Medice Arzneimittel P&#252;tter GmbH &amp; Co KG"
-             ),
-            ("h477",
-             "L03AX14",
-             "Histamine dihydrochloride",
-             "EU/1/08/477",
-             "ACTIVE",
-             "Ceplene",
-             "Laboratoires Delbert"
-             ),
-            ("o1230",
-             None,
-             "(2R,3R,4S,5R)-2-(6-amino-9H-purin-9-yl)-5-((((1r,3S)-3-(2-(5-(tert-butyl)-1H-benzo[d]imidazol-2-yl)ethyl)cyclobutyl)(isopropyl) amino)methyl)tetrahydrofuran-3,4-diol",
-             "EU/3/13/1230",
-             "ACTIVE",
-             "EU/3/13/1230",
-             "Voisin Consulting Life Sciences"
-             )
-        ]
-    )
-    def test_get_data_from_medicine_json(
-            eu_num_short,
-            exp_atc_code,
-            exp_active_substance,
-            exp_eu_pnumber,
-            exp_eu_aut_status,
-            exp_eu_brand_name_current,
-            exp_eu_mah_current
-    ):
-        html_active = ec.get_ec_html(
-            f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm")
-        medicine_json, *_ = ec.get_ec_json_objects(html_active)
-        medicine_dict, _ = ec.get_data_from_medicine_json(medicine_json)
-
-        assert medicine_dict["atc_code"] == exp_atc_code
-        assert medicine_dict["active_substance"] == exp_active_substance
-        assert medicine_dict["eu_pnumber"] == exp_eu_pnumber
-        assert medicine_dict["eu_aut_status"] == exp_eu_aut_status
-        assert medicine_dict["eu_brand_name_current"] == exp_eu_brand_name_current
-        assert medicine_dict["eu_mah_current"] == exp_eu_mah_current
-
-    @pytest.mark.parametrize(
-        "eu_num_short, "
-        "exp_eu_aut_date, "
-        "exp_aut_type_initial, "
-        "exp_aut_type_current, "
-        "exp_ema_number, "
-        "exp_ema_number_certainty",
-        [
-            (
-                    "h477",
-                    "07-10-2008",
-                    "exceptional",
-                    "",
-                    "",
-                    ""
-            )
-        ]
-    )
-    def test_get_data_from_procedures_json(
-            eu_num_short,
-            exp_eu_aut_date,
-            exp_aut_type_initial,
-            exp_aut_type_current,
-            exp_ema_number,
-            exp_ema_number_certainty
-    ):
-        html_active = ec.get_ec_html(
-            f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm")
-        _, procedures_json, *_ = ec.get_ec_json_objects(html_active)
-        procedures_dict, *_ = ec.get_data_from_procedures_json(procedures_json)
-
-        assert procedures_dict["eu_aut_date"] == exp_eu_aut_date
-        assert procedures_dict["eu_aut_type_initial"] == exp_aut_type_initial
-        # assert procedures_dict["eu_aut_type_current"] = exp_aut_type_current
-        # assert procedures_dict["ema_number"] = exp_ema_number
-        # assert procedures_dict["ema_number_certainty"] = exp_ema_number_certainty
 
     def test_determine_current_aut_type(self):
         self.fail()
 
+
     def test_determine_aut_type(self):
         self.fail()
+
 
     def test_format_ema_number(self):
         self.fail()
 
+
     def test_determine_ema_number(self):
         self.fail()
+
+
+"""
+Unit tests that make use of pytest.mark.parametrize can not be in the class , because there is a conflict between the
+unittest and pytest libraries:
+https://docs.pytest.org/en/7.1.x/how-to/unittest.html#:~:text=pytest%20supports%20running%20Python%20unittest,full%20advantage%20of%20pytest's%20features.
+
+TODO: Find a way to make the two libraries compatible
+"""
+
+# Unit test that checks whether the correct data is retrieved for a certain medicine.
+@pytest.mark.parametrize(
+    "eu_num_short, "
+    "exp_atc_code, "
+    "exp_active_substance, "
+    "exp_eu_pnumber, "
+    "exp_eu_aut_status, "
+    "exp_eu_brand_name_current, "
+    "exp_eu_mah_current, "
+    "medicine_type",
+    [
+        ("h412",
+         "B03XA01",
+         "epoetin alfa",
+         "EU/1/07/412",
+         "ACTIVE",
+         "Abseamed",
+         "Medice Arzneimittel P&#252;tter GmbH &amp; Co KG",
+         ec.MedicineType.HUMAN_USE_ACTIVE
+         ),
+        ("h477",
+         "L03AX14",
+         "Histamine dihydrochloride",
+         "EU/1/08/477",
+         "ACTIVE",
+         "Ceplene",
+         "Laboratoires Delbert",
+         ec.MedicineType.HUMAN_USE_ACTIVE
+         ),
+        ("o1230",
+         "not applicable",
+         "(2R,3R,4S,5R)-2-(6-amino-9H-purin-9-yl)-5-((((1r,3S)-3-(2-(5-(tert-butyl)-1H-benzo[d]imidazol-2-yl)ethyl)"
+         "cyclobutyl)(isopropyl) amino)methyl)tetrahydrofuran-3,4-diol",
+         "EU/3/13/1230",
+         "ACTIVE",
+         "EU/3/13/1230",
+         "Voisin Consulting Life Sciences",
+         ec.MedicineType.ORPHAN_ACTIVE
+         )
+    ]
+)
+def test_get_data_from_medicine_json(
+        eu_num_short,
+        exp_atc_code,
+        exp_active_substance,
+        exp_eu_pnumber,
+        exp_eu_aut_status,
+        exp_eu_brand_name_current,
+        exp_eu_mah_current,
+        medicine_type
+):
+    html_active = ec.get_ec_html(
+        f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm")
+    medicine_json, *_ = ec.get_ec_json_objects(html_active)
+    medicine_dict, _ = ec.get_data_from_medicine_json(medicine_json, eu_num_short, medicine_type)
+
+    assert medicine_dict["atc_code"] == exp_atc_code
+    assert medicine_dict["active_substance"] == exp_active_substance
+    assert medicine_dict["eu_pnumber"] == exp_eu_pnumber
+    assert medicine_dict["eu_aut_status"] == exp_eu_aut_status
+    assert medicine_dict["eu_brand_name_current"] == exp_eu_brand_name_current
+    assert medicine_dict["eu_mah_current"] == exp_eu_mah_current
+
+
+@pytest.mark.parametrize(
+    "eu_num_short, "
+    "exp_eu_aut_date, "
+    "exp_aut_type_initial, "
+    "exp_aut_type_current, "
+    "exp_ema_number, "
+    "exp_ema_number_certainty",
+    [
+        (
+                "h477",
+                "07-10-2008",
+                "exceptional",
+                "",
+                "",
+                ""
+        )
+    ]
+)
+def test_get_data_from_procedures_json(
+        eu_num_short,
+        exp_eu_aut_date,
+        exp_aut_type_initial,
+        exp_aut_type_current,
+        exp_ema_number,
+        exp_ema_number_certainty
+):
+    html_active = ec.get_ec_html(
+        f"https://ec.europa.eu/health/documents/community-register/html/{eu_num_short}.htm")
+    _, procedures_json, *_ = ec.get_ec_json_objects(html_active)
+    procedures_dict, *_ = ec.get_data_from_procedures_json(procedures_json, eu_num_short)
+
+    assert procedures_dict["eu_aut_date"] == exp_eu_aut_date
+    assert procedures_dict["eu_aut_type_initial"] == exp_aut_type_initial
+    # assert procedures_dict["eu_aut_type_current"] = exp_aut_type_current
+    # assert procedures_dict["ema_number"] = exp_ema_number
+    # assert procedures_dict["ema_number_certainty"] = exp_ema_number_certainty
