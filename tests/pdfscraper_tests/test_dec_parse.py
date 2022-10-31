25814afabf120020e6defced28c0ff99de21cf3c
Kawaii-Killer <84580900+Kawaii-Killer@users.noreply.github.com>
2022-10-31 07:54:27 +0100

    fix dec merge problems

--- tests/pdfscraper_tests/test_dec_parse.py
+++ tests/pdfscraper_tests/test_dec_parse.py
@@ -5,97 +5,44 @@ import fitz
 import datetime
 import scraping.pdf_module.pdf_scraper.parsers.dec_parse as dec_parse
 
-test_data_loc = "../../data/epars"
+test_data_loc = "../../../data"
 dec_txt = []
 percentage_str = "Percentage found: "
 
 
 # Tests all functions of EPAR parser with all XML files
-class TestEparParse(TestCase):
+class TestDecParse(TestCase):
     def setUp(self):
-        # Prepare a list of XML files
-        files = [file for file in os.listdir(test_data_loc) if os.path.isfile(os.path.join(test_data_loc, file))]
-        dec_files = [file for file in files if "dec" and ".pdf" and "_0" in file]
+        files = []
+        for folder in os.listdir(test_data_loc):
+            for file in os.listdir(os.path.join(test_data_loc, folder)):
+                path = os.path.join(test_data_loc, folder, file)
+                if os.path.isfile(path):
+                    files.append(path)
+        dec_files = [file for file in files if "dec_0" in file and ".pdf" in file]
         # Get XML bodies, assuming there are XML files
         for dec_file in dec_files:
-            dec_path = os.path.join(test_data_loc, dec_file)
-            pdf = fitz.open(dec_path)
+            pdf = fitz.open(dec_file)
             txt = dec_parse.get_txt_from_pdf(pdf)
             pdf.close()
-            dec_txt.append(txt)
+            dec_txt.append((txt, dec_file))
 
     # eu_prime_initial
-    def test_get_prime(self):
+    def test_get_date(self):
         # Call get_prime
-        for txt in dec_txt:
+        not_found_count = 0
+        for txt, filename in dec_txt:
             output = dec_parse.dec_get_date(txt)
-            self.assertTrue(isinstance(output, datetime.datetime))
-
-
-    # # ema_procedure_start_initial
-    # def test_get_date(self):
-    #     found_count = 0
-    #     # Call get_date
-    #     for xml_body in xml_bodies:
-    #         output = epar_parse.get_date(xml_body)
-    #         if output != "no_date_found":
-    #             found_count += 1
-    #             day = re.search(r"\d{2}/", output)[0][:2].strip()
-    #             month = re.search(r"/\d{2}/", output)[0][1:3].strip()
-    #             year = re.search(r"\d{4}", output)[0].strip()
-    #             self.check_date(day, month, year)
-    #     percentage_found = found_count / len(xml_bodies) * 100
-    #     print(percentage_str + str(round(percentage_found, 2)) + '%')
-    #     self.assertGreater(percentage_found, 90)
-
-    # # chmp_opinion_date
-    # def test_get_opinion_date(self):
-    #     found_count = 0
-    #     # Call get_opinion_date
-    #     for xml_body in xml_bodies:
-    #         output = epar_parse.get_opinion_date(xml_body)
-    #         if output != "no_chmp_found":
-    #             found_count += 1
-    #             day = re.search(r"\d{2}/", output)[0][:2].strip()
-    #             month = re.search(r"/\d{2}/", output)[0][1:3].strip()
-    #             year = re.search(r"\d{4}", output)[0].strip()
-    #             self.check_date(day, month, year)
-    #     percentage_found = found_count / len(xml_bodies) * 100
-    #     print(percentage_str + str(round(percentage_found, 2)) + '%')
-    #     self.assertGreater(percentage_found, 90)
-
-    # # Check for reasonable dates
-    # def check_date(self, day, month, year):
-    #     self.assertGreater(int(year), 1900)
-    #     # Make sure to check this in the future :D
-    #     self.assertGreater(3000, int(year))
-    #     self.assertGreater(int(month), 0)
-    #     self.assertGreater(13, int(month))
-    #     self.assertGreater(int(day), 0)
-    #     self.assertGreater(32, int(day))
-
-    # # eu_legal_basis
-    # def test_get_legal_basis(self):
-    #     found_count = 0
-    #     # Call get_legal_basis
-    #     for xml_body in xml_bodies:
-    #         output = epar_parse.get_legal_basis(xml_body)
-    #         if output != "no_legal_basis":
-    #             found_count += 1
-    #             self.assertGreater(len(output), 0)
-    #             for a in output:
-    #                 self.assertIn("article", a)
-    #     percentage_found = found_count / len(xml_bodies) * 100
-    #     print(percentage_str + str(round(percentage_found, 2)) + '%')
-    #     self.assertGreater(percentage_found, 90)
-
-    # # eu_prime_initial
-    # def test_get_prime(self):
-    #     yes_exists = False
-    #     # Call get_prime
-    #     for xml_body in xml_bodies:
-    #         output = epar_parse.get_prime(xml_body)
-    #         if output == "yes":
-    #             yes_exists = True
-    #         self.assertIn(output, 'yesno')
-    #     self.assertTrue(yes_exists)
+            self.assertTrue((isinstance(output, str) or isinstance(output, datetime.datetime)))
+            if output == datetime.datetime(1980, 1, 1, 0 ,0):
+                not_found_count += 1
+                print(f"{filename} date not found")
+            if isinstance(output, datetime.datetime):
+                self.assertGreater(datetime.datetime(2050, 1, 1, 0, 0), output)
+                self.assertGreater(output, datetime.datetime(1979, 1, 1, 0, 0))
+            if isinstance(output, str):
+                self.assertTrue(output == 'Date is blank')
+        percentage_found = (len(dec_txt)-not_found_count) / len(dec_txt) * 100
+        print(percentage_str + str(round(percentage_found, 2)) + '%')
+        print(f"Amount not found: {not_found_count}")
+        self.assertGreater(percentage_found, 99)
