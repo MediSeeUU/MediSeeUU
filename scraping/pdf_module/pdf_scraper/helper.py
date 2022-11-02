847f1b16fa9cdf30444c735761da0769d75f9c19
elioverhoef <49610595+elioverhoef@users.noreply.github.com>
2022-11-02 16:39:20 +0100

    Update helper.py

--- scraping/pdf_module/pdf_scraper/helper.py
+++ scraping/pdf_module/pdf_scraper/helper.py
@@ -112,12 +112,12 @@ def get_date(txt: str) -> datetime.datetime:
         datetime.datetime: found date.
     """
     if txt != '':
-        txt = txt.lower().split(' ')[0]
+
         try:
             return dateutil.parser.parse(txt, fuzzy=True)
         except dateutil.parser._parser.ParserError:
             pass
-
+        txt = txt.lower().split(' ')[0]
         temp_date = convert_roman_numbers(txt)
         try:
             return dateutil.parser.parse(temp_date, fuzzy=True)
