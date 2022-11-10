7b51c695112af97792c85db76f0abae3a336d1bc
Yfke Smit <smityfke@gmail.com>
2022-11-10 15:42:54 +0100

    Fix in filter_retry web: path to json for monolithic main

--- scraping/web_scraper/filter_retry.py
+++ scraping/web_scraper/filter_retry.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 from scraping.web_scraper import download, ec_scraper, ema_scraper, utils, json_helper
 from scraping.web_scraper import __main__ as m
 from scraping.filter import pdf_filter
@@ -27,10 +28,14 @@ def run_filter(n: int, data_filepath: str = "../../data"):
         n (int): Number of times filter is called
         data_filepath (str, optional): the path to the data folder. Defaults to "../../data"
     """
+    json_path = "web_scraper/"
+    # If file is run locally:
+    if "web_scraper" in os.getcwd():
+        json_path = ""
     for i in range(n):
         pdf_filter.filter_all_pdfs(data_filepath)
-        url_file = json_helper.JsonHelper(path="JSON/urls.json").load_json()
-        retry_all('filter.txt', url_file)
+        url_file = json_helper.JsonHelper(path=f"{json_path}JSON/urls.json").load_json()
+        retry_all('filter.txt', url_file, data_filepath)
 
 
 def retry_all(filter_path: str, urls_file: dict[str, dict[str, list[str]]], data_filepath: str = "../../data"):
@@ -52,21 +57,23 @@ def retry_all(filter_path: str, urls_file: dict[str, dict[str, list[str]]], data
             if eu_n in urls_file:
                 m.get_urls_ec(urls_file[eu_n]["ec_url"], eu_n, med_type_dict[''.join(filename_elements[:2])],
                               data_filepath)
-                retry_download(eu_n, filename_elements, urls_file[eu_n])
+                retry_download(eu_n, filename_elements, urls_file[eu_n], data_filepath)
 
 
-def retry_download(eu_n: str, filename_elements: list[str], url_dict: dict[str, list[str]]):
+def retry_download(eu_n: str, filename_elements: list[str], url_dict: dict[str, list[str]], data_filepath: str):
     """
     Calls the download function for a specific file
     Args:
         eu_n (str): eu number used for calling the download function
         filename_elements (list[str]): filename elements, used as parameter in the download function
         url_dict (dict[str, list[str]] | dict[str, str]): dictionary that contains the url where to download from
+        data_filepath (str): the path to the data folder
     """
     url = url_dict[key_dict[filename_elements[2]]]
     if len(filename_elements) == 4:
         url = url[int(filename_elements[3])]
-    utils.exception_retry(download.download_pdf_from_url, logging_instance=log)(url, eu_n, filename_elements)
+    utils.exception_retry(download.download_pdf_from_url, logging_instance=log)(url, eu_n, filename_elements,
+                                                                                data_filepath, overwrite=True)
 
 
 # used for testing
