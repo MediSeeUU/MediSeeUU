1d9f1125695c3e60a2fdeccfcf223dab3faf2afe
elioverhoef <49610595+elioverhoef@users.noreply.github.com>
2022-11-15 12:30:44 +0100

    Automatic scraping

--- scraping/__main__.py
+++ scraping/__main__.py
@@ -1,6 +1,7 @@
-import sys
 import os.path as path
 import os
+import pause
+from datetime import datetime, timedelta
 
 import web_scraper.__main__ as web_scraper
 import pdf_module.pdf_scraper.__main__ as pdf_scraper
@@ -10,15 +11,25 @@ import db_communicator.__main__ as db_communicator
 
 def main():
     """
-    Runs the entire scraping process on the specified directory
+    Runs the entire scraping process on the specified directory every week
+    """
+    run = True
+    while run:
+        start_time = datetime.now()
+        run_all()
+        pause.until(start_time + timedelta(days=7))  # Wait for seven days until the next scrape cycle
+
+
+def run_all():
+    """
+    Runs all modules of MediSee
+    For now only the web_scraper and pdf_scraper will be run.
     """
     data_folder_directory = '../data'
-    # print(data_folder_directory)
     if not path.isdir(data_folder_directory):
         os.mkdir(data_folder_directory)
 
-    # For now only the web_scraper and pdf_scraper will be run
-    # web_scraper.main(data_folder_directory)
+    web_scraper.main(data_folder_directory)
     pdf_scraper.main(data_folder_directory)
     # combiner.main(data_folder_directory)
     # db_communicator_main.main(data_folder_directory)
