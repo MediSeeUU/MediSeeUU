66b33c2dd90e6bf37665e8c290fc1b32a092b7ba
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-11-16 12:14:45 +0100

    Update medicine.py

--- medctrl-backend/API/api/serializers/medicine_serializers/scraper/medicine.py
+++ medctrl-backend/API/api/serializers/medicine_serializers/scraper/medicine.py
@@ -4,6 +4,12 @@
 from rest_framework import serializers
 from api.models.medicine_models import (
     MedicinalProduct,
+    IngredientsAndSubstances,
+    MarketingAuthorisation,
+    AcceleratedAssessment,
+    Duration,
+    OrphanProduct,
+    Procedures,
     LegalBases,
 )
 
@@ -22,9 +28,93 @@ class MedicineSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class IngredientsAndSubstancesSerializer(serializers.ModelSerializer):
+    """
+    IngredientsAndSubstances table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = IngredientsAndSubstances
+        fields = "__all__"
+
+
+class MarketingAuthorisationSerializer(serializers.ModelSerializer):
+    """
+    MarketingAuthorisation table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = MarketingAuthorisation
+        fields = "__all__"
+
+
+class AcceleratedAssessmentSerializer(serializers.ModelSerializer):
+    """
+    AcceleratedAssessment table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = AcceleratedAssessment
+        exclude = ["id", ]
+
+
+class DurationSerializer(serializers.ModelSerializer):
+    """
+    Duration table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = Duration
+        exclude = ["id", ]
+
+
+class OrphanProductSerializer(serializers.ModelSerializer):
+    """
+    OrphanProduct table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = OrphanProduct
+        fields = "__all__"
+
+
+class ProceduresSerializer(serializers.ModelSerializer):
+    """
+    Procedures table serializer for the scraper endpoints
+    """
+
+    class Meta:
+        """
+        Meta information
+        """
+
+        model = Procedures
+        exclude = ["id", ]
+
+
 class LegalBasesSerializer(serializers.ModelSerializer):
     """
-    Medicine table serializer for the scraper endpoints
+    LegalBases table serializer for the scraper endpoints
     """
 
     class Meta:
