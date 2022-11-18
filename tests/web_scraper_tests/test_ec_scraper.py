838223f767e8a3bbd931ddf4a1f45555db9654e0
TiKee99 <44522314+TiKee99@users.noreply.github.com>
2022-11-19 00:41:08 +0100

    Merge: Fixed tests, except integration tests

--- tests/web_scraper_tests/test_ec_scraper.py
+++ tests/web_scraper_tests/test_ec_scraper.py
@@ -92,7 +92,6 @@ class TestEcScraper(unittest.TestCase):
         [
             "h477",
             "2008-10-07 00:00:00",
-            "2008-10-07 00:00:00",
             "exceptional",
             "",
             "",
@@ -149,30 +148,30 @@ class TestEcScraper(unittest.TestCase):
                'http://www.ema.europa.eu/ema/index.jsp?curl=pages/medicines/human/orphans/2009/11/human_orphan_000281'
                '.jsp&murl=menus/medicines/medicines.jsp&mid=WC0b01ac058001d12b']
         decision = [
-            ('https://ec.europa.eu/health/documents/community-register/2004/200404287648/dec_7648_en.pdf', 0),
-            ('https://ec.europa.eu/health/documents/community-register/2006/2006022811168/dec_11168_en.pdf', 4),
-            ('https://ec.europa.eu/health/documents/community-register/2007/2007042623649/dec_23649_en.pdf', 5),
-            ('https://ec.europa.eu/health/documents/community-register/2009/2009032555652/dec_55652_en.pdf', 6),
-            ('https://ec.europa.eu/health/documents/community-register/2011/20111003110571/dec_110571_en.pdf', 12),
-            ('https://ec.europa.eu/health/documents/community-register/2012/20120823124192/dec_124192_en.pdf', 15),
-            ('https://ec.europa.eu/health/documents/community-register/2014/20140603128801/dec_128801_en.pdf', 19),
-            ('https://ec.europa.eu/health/documents/community-register/2015/20150226130899/dec_130899_en.pdf', 20),
-            ('https://ec.europa.eu/health/documents/community-register/2017/20170516137848/dec_137848_en.pdf', 22),
-            ('https://ec.europa.eu/health/documents/community-register/2019/20190919146015/dec_146015_en.pdf', 24),
-            ('https://ec.europa.eu/health/documents/community-register/2021/20210322150898/dec_150898_en.pdf', 26)
+            'https://ec.europa.eu/health/documents/community-register/2004/200404287648/dec_7648_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2006/2006022811168/dec_11168_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2007/2007042623649/dec_23649_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2009/2009032555652/dec_55652_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2011/20111003110571/dec_110571_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2012/20120823124192/dec_124192_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2014/20140603128801/dec_128801_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2015/20150226130899/dec_130899_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2017/20170516137848/dec_137848_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2019/20190919146015/dec_146015_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2021/20210322150898/dec_150898_en.pdf'
         ]
         annex = [
-            ('https://ec.europa.eu/health/documents/community-register/2004/200404287648/anx_7648_en.pdf', 0),
-            ('https://ec.europa.eu/health/documents/community-register/2006/2006022811168/anx_11168_en.pdf', 4),
-            ('https://ec.europa.eu/health/documents/community-register/2007/2007042623649/anx_23649_en.pdf', 5),
-            ('https://ec.europa.eu/health/documents/community-register/2009/2009032555652/anx_55652_en.pdf', 6),
-            ('https://ec.europa.eu/health/documents/community-register/2011/20111003110571/anx_110571_en.pdf', 12),
-            ('https://ec.europa.eu/health/documents/community-register/2012/20120823124192/anx_124192_en.pdf', 15),
-            ('https://ec.europa.eu/health/documents/community-register/2014/20140603128801/anx_128801_en.pdf', 19),
-            ('https://ec.europa.eu/health/documents/community-register/2015/20150226130899/anx_130899_en.pdf', 20),
-            ('https://ec.europa.eu/health/documents/community-register/2017/20170516137848/anx_137848_en.pdf', 22),
-            ('https://ec.europa.eu/health/documents/community-register/2019/20190919146015/anx_146015_en.pdf', 24),
-            ('https://ec.europa.eu/health/documents/community-register/2021/20210322150898/anx_150898_en.pdf', 26)
+            'https://ec.europa.eu/health/documents/community-register/2004/200404287648/anx_7648_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2006/2006022811168/anx_11168_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2007/2007042623649/anx_23649_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2009/2009032555652/anx_55652_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2011/20111003110571/anx_110571_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2012/20120823124192/anx_124192_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2014/20140603128801/anx_128801_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2015/20150226130899/anx_130899_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2017/20170516137848/anx_137848_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2019/20190919146015/anx_146015_en.pdf',
+            'https://ec.europa.eu/health/documents/community-register/2021/20210322150898/anx_150898_en.pdf'
         ]
         html_active: requests.Response = utils.get_html_object(url)
         dec_result, anx_result, ema_result, _ = ec.scrape_medicine_page(url, html_active,
