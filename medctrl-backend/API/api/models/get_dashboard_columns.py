938efb099119d299d1a0210a7f065c781db89a5e
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-12-02 13:36:11 +0100

    Update get_dashboard_columns.py

--- medctrl-backend/API/api/models/get_dashboard_columns.py
+++ medctrl-backend/API/api/models/get_dashboard_columns.py
@@ -2,7 +2,7 @@ from django.db import models
 from api.models.create_dashboard_columns import DashBoardHistoryInitialColumn
 
 
-def get_all_initial_history_columns(model_list: list[models.Model]) -> list[str]:
+def get_initial_history_columns(model: models.Model) -> list[str]:
     """
 
     Args:
@@ -12,10 +12,9 @@ def get_all_initial_history_columns(model_list: list[models.Model]) -> list[str]
 
     """
     result = []
-    for model in model_list:
-        for field in model._meta.get_fields():
-            if hasattr(field, "dashboard_columns"):
-                for dashboard_column in field.dashboard_columns:
-                    if isinstance(dashboard_column, DashBoardHistoryInitialColumn):
-                        result.append(field.name)
+    for field in model._meta.get_fields():
+        if hasattr(field, "dashboard_columns"):
+            for dashboard_column in field.dashboard_columns:
+                if isinstance(dashboard_column, DashBoardHistoryInitialColumn):
+                    result.append(field.name)
     return result
