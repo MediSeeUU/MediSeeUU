fe5841a136fefa05294799ca6a10fa9bfb677a9b
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2022-12-05 17:27:37 +0100

    added debugging tool that compiles all combine.json files in data folder recursively

--- scraping/utilities/debugging_tools/data_compiler.py
+++ scraping/utilities/debugging_tools/data_compiler.py
@@ -1,5 +1,9 @@
-import scraping.utilities.json.json_compiler
+import scraping.utilities.json.json_compiler as jc
 
 
-def main():
-    pass
+# def main():
+#     pass
+
+compile_dir = "D:\Git_repos\MediSeeUU\data"
+save_dir = "D:\Git_repos\MediSeeUU\data"
+jc.compile_json_file(compile_dir, save_dir, ["combine"], [], True)
