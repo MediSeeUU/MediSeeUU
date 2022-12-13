120d1e28ea9f5fa43e681aeb5327badc30b490f1
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-12-13 09:47:06 +0100

    Update history_authorisation_type.py

--- medctrl-backend/API/api/models/human_models/history_authorisation_type.py
+++ medctrl-backend/API/api/models/human_models/history_authorisation_type.py
@@ -34,7 +34,7 @@ class HistoryAuthorisationType(models.Model):
 
     eu_aut_type = create_dashboard_history_current_column(
         models.CharField(
-            max_length=11,
+            max_length=26,
             choices=AutTypes.choices,
             null=False,
             blank=False,
