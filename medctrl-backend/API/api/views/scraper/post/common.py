cb4dc958ec1326933909119b7285c9584bb79732
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2022-12-14 14:17:53 +0100

    initial histories are now added to db

--- medctrl-backend/API/api/views/scraper/post/common.py
+++ medctrl-backend/API/api/views/scraper/post/common.py
@@ -1,3 +1,10 @@
+# This program has been developed by students from the bachelor Computer Science at
+# Utrecht University within the Software Project course.
+# © Copyright Utrecht University (Department of Information and Computing Sciences)
+
+from api.models.get_dashboard_columns import get_initial_history_columns
+
+
 def insert_data(data, current, serializer):
     """
 
@@ -14,3 +21,12 @@ def insert_data(data, current, serializer):
         return inserted.pk
     else:
         raise ValueError(medicine_serializer.errors)
+
+
+def pop_initial_histories_data(data, models):
+    initial_history_data = {}
+    for initial_history_column in get_initial_history_columns(models):
+        if initial_history_column in data:
+            initial_history_data[initial_history_column] = \
+                data.pop(initial_history_column)
+    return initial_history_data
