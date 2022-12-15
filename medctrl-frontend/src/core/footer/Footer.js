71130dd5a02d4b00cb9bf0e770e755a4ba9fc04b
Yfke Smit <smityfke@gmail.com>
2022-12-15 12:37:47 +0100

    github button now has a hover function

--- medctrl-frontend/src/core/footer/Footer.js
+++ medctrl-frontend/src/core/footer/Footer.js
@@ -46,9 +46,23 @@ function Footer() {
         </ul>
       </section>
 
-      <div class="github_button">
-        <a href="https://github.com/MediSeeUU/MediSeeUU/issues" target="_blank" rel="noreferrer">
-          <img src={github_button} id="github_button" />
+      <div class="med-footer-github-button">
+        <a  href="https://github.com/MediSeeUU/MediSeeUU/issues" 
+            target="_blank" 
+            title="Incorrect data or bug in website?\nClick here to raise a Github issue"
+            rel="noreferrer">
+          <svg  width="40px" 
+                height="40px" 
+                viewBox="0 0 40 40" 
+                id="github_button" 
+                xmlns="http://www.w3.org/2000/svg">
+                  <title>Incorrect data or bug in website? &#10;&#13;Click here to raise a Github issue</title>
+                  <g id="exclamation-circle-Regular">
+                    <path id="exclamation-circle-Regular-2" 
+                    data-name="exclamation-circle-Regular" 
+                    d="M20 3.75A16.25 16.25 0 1 0 36.25 20 16.268 16.268 0 0 0 20 3.75Zm0 30A13.75 13.75 0 1 1 33.75 20 13.765 13.765 0 0 1 20 33.75ZM21.667 26.667a1.667 1.667 0 1 1 -1.667 -1.667A1.667 1.667 0 0 1 21.667 26.667Zm-2.917 -6.667V13.333a1.25 1.25 0 0 1 2.5 0v6.667a1.25 1.25 0 0 1 -2.5 0Z"/>
+                  </g>
+          </svg>
         </a>
       </div>
 
