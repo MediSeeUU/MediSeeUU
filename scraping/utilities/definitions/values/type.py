903fd77096ade811f3b418cd134dad2cdb1e624b
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2022-12-19 13:33:36 +0100

    fixed run errors, temporarily disabled default_date and default_datetime

--- scraping/utilities/definitions/values/type.py
+++ scraping/utilities/definitions/values/type.py
@@ -34,16 +34,16 @@ float_not_applicable = numeric_not_applicable
 float_should_be_present = numeric_should_be_present
 
 #dates
-default_date = datetime.datetime.min.date()
-default_date_str = str(datetime.datetime.min.date())
+# default_date = datetime.datetime.min.date()
+# default_date_str = str(datetime.datetime.min.date())
 date_not_found = generic_not_found
 date_not_applicable = generic_not_applicable
 date_should_be_present = generic_should_be_present
 date_left_blank = "date is left blank in document"
 
 #datetimes
-default_datetime = datetime.date.min.day()
-default_datetime_str = str(datetime.date.min.day())
+# default_datetime = datetime.date.min.day()
+# default_datetime_str = str(datetime.date.min.day())
 datetime_not_found = generic_not_found
 datetime_not_applicable = generic_not_applicable
 datetime_should_be_present = generic_should_be_present
