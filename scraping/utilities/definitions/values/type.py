c5d5fa58751f487d63481049c44bd762eea0b768
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2022-12-21 17:17:49 +0100

    hopefully fixed errors

--- scraping/utilities/definitions/values/type.py
+++ scraping/utilities/definitions/values/type.py
@@ -34,19 +34,19 @@ float_not_applicable = numeric_not_applicable
 float_should_be_present = numeric_should_be_present
 
 #dates
-# default_date = datetime.datetime.min.date()
-# default_date_str = str(datetime.datetime.min.date())
+# default_date = datetime.date.min()
+# default_date_str = str(datetime.date.min())
 date_not_found = generic_not_found
-date_history_not_found = str(datetime.datetime.min.date())
+date_history_not_found = str(datetime.date.min())
 date_not_applicable = generic_not_applicable
 date_should_be_present = generic_should_be_present
 date_left_blank = "date is left blank in document"
 
 #datetimes
-# default_datetime = datetime.date.min.day()
-# default_datetime_str = str(datetime.date.min.day())
+# default_datetime = datetime.datetime.min()
+# default_datetime_str = str(datetime.datetime.min())
 datetime_not_found = generic_not_found
-datetime_history_not_found = str(datetime.datetime.min.date())
+datetime_history_not_found = str(datetime.datetime.min())
 datetime_not_applicable = generic_not_applicable
 datetime_should_be_present = generic_should_be_present
 datetime_left_blank = "date is left blank in document"
