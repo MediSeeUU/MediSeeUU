f94c4a6a1a92da03af5329ed8d8558f0c69dbec4
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2023-01-09 10:31:46 +0100

    update eu_aut_status

--- backend/API/api/models/common.py
+++ backend/API/api/models/common.py
@@ -40,8 +40,8 @@ class AutTypes(models.TextChoices, metaclass=NAChoices):
     """
     Choice types for eu_aut_type. Is derived from the enumerated choice class.
     """
-    CONDITIONAL = "conditional",
-    EXCEPTIONAL = "exceptional",
+    CONDITIONAL = "conditional"
+    EXCEPTIONAL = "exceptional"
     STANDARD = "standard"
     UNCERTAIN = "exceptional or conditional"
 
@@ -50,19 +50,21 @@ class AutStatus(models.TextChoices, metaclass=NAChoices):
     """
     Choice types for eu_aut_status. Is derived from the enumerated choice class.
     """
-    ACTIVE = "ACTIVE",
-    WITHDRAWAL = "WITHDRAWN",
-    REFUSALS = "REFUSED"
+    ACTIVE = "ACTIVE"
+    WITHDRAWN = "WITHDRAWN"
+    REFUSED = "REFUSED"
+    NOT_RENEWED = "NOT_RENEWED"
+    EXPIRED = "EXPIRED"
 
 
 class LegalBasesTypes(models.TextChoices, metaclass=NAChoices):
     """
     Choice types for legal bases. Is derived from the enumerated choice class.
     """
-    article4_8 = "article 4.8",
-    article4_8_1 = "article 4.8.1",
-    article4_8_2 = "article 4.8.2",
-    article4_8_3 = "article 4.8.3",
+    article4_8 = "article 4.8"
+    article4_8_1 = "article 4.8.1"
+    article4_8_2 = "article 4.8.2"
+    article4_8_3 = "article 4.8.3"
     article8_3 = "article 8.3"
     article_10_1 = "article 10.1"
     article_10_2 = "article 10.2"
