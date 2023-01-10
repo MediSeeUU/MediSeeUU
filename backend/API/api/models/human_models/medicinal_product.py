a5cb00a562c69cdd110f1f285fc5e4cb19ed15ce
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2023-01-10 09:57:51 +0100

    removed eu_pnumber_id and ema_number_id

--- backend/API/api/models/human_models/medicinal_product.py
+++ backend/API/api/models/human_models/medicinal_product.py
@@ -52,15 +52,6 @@ class MedicinalProduct(models.Model):
         Category.Marketing_authorisation,
         DataFormats.String,
         "EU Product Number",
-        [
-            ExtraDashBoardColumn(
-                Category.Marketing_authorisation,
-                "eu_pnumber_id",
-                DataFormats.Number,
-                "EU product number ID",
-                lambda x: int(x[-3:]),
-            )
-        ]
     )
 
     ema_url = create_dashboard_column(
@@ -92,15 +83,6 @@ class MedicinalProduct(models.Model):
         Category.Medicinal_product,
         DataFormats.String,
         "EMA Application Number",
-        [
-            ExtraDashBoardColumn(
-                Category.Medicinal_product,
-                "ema_number_id",
-                DataFormats.String,
-                "EMA application number ID",
-                lambda x: int(x[-3:]),
-            )
-        ]
     )
 
     eu_med_type = create_dashboard_column(
