7df3304ad6db10fd960675fdee1ee410f63471a7
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2023-01-11 14:23:09 +0100

    improved create_partner_user

--- backend/API/api/management/commands/create_partner_user.py
+++ backend/API/api/management/commands/create_partner_user.py
@@ -32,16 +32,24 @@ class Command(BaseCommand):
 
         """
 
-        username = input("Enter a username: ")
-        password = getpass.getpass("Enter a password: ")
+        while not (username := input("Enter a username: ")) or User.objects.filter(username=username).first():
+            if username:
+                self.stdout.write(self.style.NOTICE("Username already exists"))
+            else:
+                self.stdout.write(self.style.NOTICE("Please enter a valid username"))
+
+        while not (password := getpass.getpass("Enter a password: ")):
+            self.stdout.write(self.style.NOTICE("Please enter a valid password"))
 
         # Create user
         hash_password = make_password(password)
-        scraper, created = User.objects.update_or_create(username=username, password=hash_password)
-        logging.getLogger(__name__).info(f"{username} user {self.get_action(created)}")
+        partner_user = User.objects.create_user(username=username, password=hash_password)
 
         # Assign permissions to scraper user
         for model in models:
             ct = ContentType.objects.filter(model=model.__name__).first()
             all_permissions = Permission.objects.filter(content_type=ct)
-            scraper.user_permissions.add(*all_permissions)
+            partner_user.user_permissions.add(*all_permissions)
+
+        logging.getLogger(__name__).info(f"{username} user has been created")
+        self.stdout.write(self.style.SUCCESS('Successfully created partner user "%s"' % username))
