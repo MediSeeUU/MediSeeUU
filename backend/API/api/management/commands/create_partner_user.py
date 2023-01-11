2922d58d176b9913a5a91d0f0dc585efb1d1109d
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2023-01-11 15:31:40 +0100

    improved create_partner_user

--- backend/API/api/management/commands/create_partner_user.py
+++ backend/API/api/management/commands/create_partner_user.py
@@ -17,13 +17,6 @@ This file is responsible for creating partner users. Partner users have permissi
 
 
 class Command(BaseCommand):
-
-    def get_action(self, created):
-        """
-        Returns the action that has been performed on the model
-        """
-        return "has been created" if created else "already exists"
-
     def handle(self, *args, **options):
         """
         Takes a username and password and creates a partner user. Partner users have permissions to change data in the database
@@ -32,14 +25,21 @@ class Command(BaseCommand):
 
         """
 
-        while not (username := input("Enter a username: ")) or User.objects.filter(username=username).first():
+        while not (username := input("Username: ")) or User.objects.filter(username=username).first():
             if username:
-                self.stdout.write(self.style.NOTICE("Username already exists"))
+                self.stdout.write(self.style.NOTICE("Error: That username is already taken."))
             else:
-                self.stdout.write(self.style.NOTICE("Please enter a valid username"))
+                self.stdout.write(self.style.NOTICE("Error: This field cannot be blank."))
 
-        while not (password := getpass.getpass("Enter a password: ")):
-            self.stdout.write(self.style.NOTICE("Please enter a valid password"))
+        password = password2 = None
+        while password != password2 or not password:
+            while not (password := getpass.getpass("Password: ")):
+                self.stdout.write(self.style.NOTICE("Error: Blank passwords aren't allowed."))
+
+            while not (password2 := getpass.getpass("Password (again): ")):
+                self.stdout.write(self.style.NOTICE("Error: Blank passwords aren't allowed."))
+            if password != password2:
+                self.stdout.write(self.style.NOTICE("Error: Your passwords didn't match."))
 
         # Create user
         hash_password = make_password(password)
@@ -51,5 +51,5 @@ class Command(BaseCommand):
             all_permissions = Permission.objects.filter(content_type=ct)
             partner_user.user_permissions.add(*all_permissions)
 
-        logging.getLogger(__name__).info(f"{username} user has been created")
-        self.stdout.write(self.style.SUCCESS('Successfully created partner user "%s"' % username))
+        logging.getLogger(__name__).info(f"{username} partner user has been created")
+        self.stdout.write(self.style.SUCCESS(f"Successfully created partner user \"{username}\""))
